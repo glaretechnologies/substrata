7aee885a7e37cdef01679e9f3993150d7c6c97c8
Nicholas Chapman <nick@indigorenderer.com>
2017-04-16 18:10:26 +0000

    Added UserID.h.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@9259 20aa11e4-6e6f-594c-a032-6291ace9ee65

---
+++ shared/UserID.h
@@ -0,0 +1,59 @@
+/*=====================================================================
+UserID.h
+--------
+File created by ClassTemplate on Thu May 05 01:07:24 2005
+Code By Nicholas Chapman.
+=====================================================================*/
+#pragma once
+
+
+#include <Platform.h>
+#include <OutStream.h>
+#include <InStream.h>
+#include <StringUtils.h>
+#include <limits>
+#include <string>
+
+
+/*=====================================================================
+UserID
+------
+Basically an index into user array on the server.
+=====================================================================*/
+class UserID
+{
+public:
+	UserID() : v(invalidUserID().value()) {}
+	explicit UserID(uint32 v_) : v(v_) {}
+
+	static UserID invalidUserID() { return UserID(std::numeric_limits<uint32>::max()); }
+
+	uint32 value() const { return v; }
+
+	bool valid() const { return v != invalidUserID().value(); }
+
+	const std::string toString() const { return ::toString(v); }
+
+	bool operator == (const UserID& other) const { return v == other.v; }
+	bool operator != (const UserID& other) const { return v != other.v; }
+
+	bool operator < (const UserID& other) const { return v < other.v; }
+	bool operator >= (const UserID& other) const { return v >= other.v; }
+
+//private:
+	uint32 v;
+};
+
+
+inline void writeToStream(const UserID& uid, OutStream& stream)
+{
+	stream.writeData(&uid.v, sizeof(uid.v));
+}
+
+
+inline UserID readUserIDFromStream(InStream& stream)
+{
+	UserID uid;
+	stream.readData(&uid.v, sizeof(uid.v));
+	return uid;
+}
