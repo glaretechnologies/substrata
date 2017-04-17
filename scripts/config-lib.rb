f66bbdbc22d271da40c7ebef936f251f6c9e41b1
Nicholas Chapman <nick@indigorenderer.com>
2017-04-17 01:04:24 +0000

    Added back config-lib.rb and config.rb, were needed in build_dist.rb. Removed some uneeded stuff from them.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@9270 20aa11e4-6e6f-594c-a032-6291ace9ee65

---
+++ scripts/config-lib.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+# This file defines configuration options to be used instead of hardcoded values.
+# The version of Qt we use needs to be accessed by both CMake and ruby build_dist script etc..#
+
+#require './script_utils.rb'
+
+# The config options
+$qt_version = "5.7.0" if OS.windows?
+$qt_version = "5.8.0" if OS.mac?
+$qt_version = "5.8.0" if OS.linux?
+
+# Get Qt path.
+indigo_libs_dir = ENV['INDIGO_LIBS']
+if indigo_libs_dir.nil?
+	puts "INDIGO_LIBS env var not defined."
+	exit(1)
+end
+
+indigo_qt_base_dir = "#{indigo_libs_dir}/Qt"
+
+$indigo_qt_dir = ""
+if OS.unix?
+	$indigo_qt_dir = "#{indigo_qt_base_dir}/#{$qt_version}"
+else
+	$indigo_qt_dir = "#{indigo_qt_base_dir}/#{$qt_version}-vs#{$vs_version}-64"
+end
---
+++ scripts/config.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+# This is a utility program to be able to use the config
+# from config-lib.rb in non ruby scripts by reading the output
+# as a string.
+# The version of Qt we use needs to be accessed by both CMake and ruby build_dist script etc..
+
+require './script_utils.rb'
+require './config-lib.rb'
+
+
+arg_parser = ArgumentParser.new(ARGV)
+
+arg_parser.options.each do |opt|
+	if opt[0] == "--qtversion"
+		puts $qt_version
+		exit 0
+	elsif opt[0] == "--qtdir"
+		puts $indigo_qt_dir
+		exit 0
+	else
+		puts "Unrecognised argument: #{opt[0]}"
+		exit 1
+	end
+end
