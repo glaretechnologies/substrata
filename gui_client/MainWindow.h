80e7058d1efd711ba3b055252fa4808c350b430b
Nicholas Chapman <nick@indigorenderer.com>
2017-05-19 01:23:49 +0000

    Compile fix.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@9412 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- gui_client/MainWindow.h
+++ gui_client/MainWindow.h
@@ -70,7 +70,7 @@ private:
 	void deleteSelectedObject();
 	void deselectObject();
 	GLObjectRef makeNameTagGLObject(const std::string& nametag);
-	Reference<OpenGLTexture> MainWindow::makeHypercardTexMap(const std::string& content);
+	Reference<OpenGLTexture> makeHypercardTexMap(const std::string& content);
 	void loadModelForObject(WorldObject* ob, bool start_downloading_missing_files);
 	void print(const std::string& message); // Print to log and console
 
