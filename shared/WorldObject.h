35f2a428f7d88e8d63138beb26ded853459799bf
Nicholas Chapman <nick@indigorenderer.com>
2017-05-24 00:10:46 +0000

    Bumped version to 0.13, bumped protocol version.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@9422 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- shared/WorldObject.h
+++ shared/WorldObject.h
@@ -23,7 +23,7 @@ class WinterShaderEvaluator;
 
 
 const uint32 CyberspaceHello = 1357924680;
-const uint32 CyberspaceProtocolVersion = 7;
+const uint32 CyberspaceProtocolVersion = 8;
 const uint32 ClientProtocolOK		= 10000;
 const uint32 ClientProtocolTooOld	= 10001;
 const uint32 ClientProtocolTooNew	= 10002;
