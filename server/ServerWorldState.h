539049b991876d4dc3f97500e077cf2d96025ace
Nicholas Chapman <nick@indigorenderer.com>
2019-08-25 11:52:35 +0000

    Moved mysql database loading stuff to ServerWorldState.cpp.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@11536 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- server/ServerWorldState.h
+++ server/ServerWorldState.h
@@ -32,6 +32,8 @@ public:
 	void readFromDisk(const std::string& path);
 	void serialiseToDisk(const std::string& path);
 	void denormaliseData(); // Build cached/denormalised fields like creator_name
+
+	void updateFromDatabase();
 	
 	UID getNextObjectUID(); // Gets and then increments next_object_uid
 	UID getNextAvatarUID(); // Gets and then increments next_avatar_uid.  Locks mutex.
