9875d83f25ccfb83ea1538444e347a305b6d0813
Nicholas Chapman <nick@indigorenderer.com>
2020-03-21 14:53:04 +0000

    Bumped protocol version.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@11989 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- shared/Protocol.h
+++ shared/Protocol.h
@@ -14,7 +14,7 @@ namespace Protocol
 
 
 const uint32 CyberspaceHello = 1357924680;
-const uint32 CyberspaceProtocolVersion = 18;
+const uint32 CyberspaceProtocolVersion = 19;
 const uint32 ClientProtocolOK		= 10000;
 const uint32 ClientProtocolTooOld	= 10001;
 const uint32 ClientProtocolTooNew	= 10002;
