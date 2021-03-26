768edf045fab9a946277b1d698c38b1c1cff3328
Nicholas Chapman <nick@indigorenderer.com>
2021-03-26 08:34:36 +0000

    Compile fix for CVBot.cpp with TLS stuff, added exception catching also.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@14528 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- cv_bot/CVBot.cpp
+++ cv_bot/CVBot.cpp
@@ -18,6 +18,7 @@ Copyright Glare Technologies Limited 2019 -
 #include <OpenSSL.h>
 #include <JSONParser.h>
 #include <Exception.h>
+#include <tls.h>
 
 
 const int server_port = 7600;
@@ -25,72 +26,90 @@ const int server_port = 7600;
 
 int main(int argc, char* argv[])
 {
-	Clock::init();
-	Networking::createInstance();
-	PlatformUtils::ignoreUnixSignals();
-	OpenSSL::init();
-	TLSSocket::initTLS();
-
-	ThreadSafeQueue<Reference<ThreadMessage> > msg_queue;
-
-	Reference<WorldState> world_state = new WorldState();
-
-	Reference<ClientThread> client_thread = new ClientThread(
-		&msg_queue,
-		//"substrata.info",
-		"localhost",
-		server_port, // port
-		"sdfsdf", // avatar URL
-		"cryptovoxels" // world name
-	);
-	client_thread->world_state = world_state;
-
-	ThreadManager client_thread_manager;
-	client_thread_manager.addThread(client_thread);
-
-	const std::string appdata_path = PlatformUtils::getOrCreateAppDataDirectory("Cyberspace");
-	const std::string resources_dir = appdata_path + "/resources";
-	conPrint("resources_dir: " + resources_dir);
-	Reference<ResourceManager> resource_manager = new ResourceManager(resources_dir);
-
-
-	// Make LogInMessage packet and enqueue to send
+	try
 	{
-		SocketBufferOutStream packet(SocketBufferOutStream::DontUseNetworkByteOrder);
-		packet.writeUInt32(Protocol::LogInMessage);
-		packet.writeStringLengthFirst("cryptovoxels");
-		packet.writeStringLengthFirst("MQqpGu9L");
+		Clock::init();
+		Networking::createInstance();
+		PlatformUtils::ignoreUnixSignals();
+		OpenSSL::init();
+		TLSSocket::initTLS();
 
-		client_thread->enqueueDataToSend(packet);
+		ThreadSafeQueue<Reference<ThreadMessage> > msg_queue;
+
+		Reference<WorldState> world_state = new WorldState();
+
+
+		// Create and init TLS client config
+		struct tls_config* client_tls_config = tls_config_new();
+		if(!client_tls_config)
+			throw glare::Exception("Failed to initialise TLS (tls_config_new failed)");
+		tls_config_insecure_noverifycert(client_tls_config); // TODO: Fix this, check cert etc..
+		tls_config_insecure_noverifyname(client_tls_config);
+
+
+		Reference<ClientThread> client_thread = new ClientThread(
+			&msg_queue,
+			"substrata.info",
+			//"localhost",
+			server_port, // port
+			"sdfsdf", // avatar URL
+			"cryptovoxels", // world name
+			client_tls_config
+		);
+		client_thread->world_state = world_state;
+
+		ThreadManager client_thread_manager;
+		client_thread_manager.addThread(client_thread);
+
+		const std::string appdata_path = PlatformUtils::getOrCreateAppDataDirectory("Cyberspace");
+		const std::string resources_dir = appdata_path + "/resources";
+		conPrint("resources_dir: " + resources_dir);
+		Reference<ResourceManager> resource_manager = new ResourceManager(resources_dir);
+
+
+		// Make LogInMessage packet and enqueue to send
+		{
+			SocketBufferOutStream packet(SocketBufferOutStream::DontUseNetworkByteOrder);
+			packet.writeUInt32(Protocol::LogInMessage);
+			packet.writeStringLengthFirst("cryptovoxels");
+			packet.writeStringLengthFirst("MQqpGu9L");
+
+			client_thread->enqueueDataToSend(packet);
+		}
+
+		// Send GetAllObjects msg
+		{
+			SocketBufferOutStream packet(SocketBufferOutStream::DontUseNetworkByteOrder);
+			packet.writeUInt32(Protocol::GetAllObjects);
+			client_thread->enqueueDataToSend(packet);
+		}
+
+		// Wait until we have received all object data.
+		conPrint("Waiting for initial data to be received");
+		while(!client_thread->all_objects_received)
+		{
+			PlatformUtils::Sleep(100);
+			conPrintStr(".");
+		}
+
+		conPrint("Received objects.  world_state->objects.size(): " + toString(world_state->objects.size()));
+
+		conPrint("===================== Loading CryptoVoxels data =====================");
+		CryptoVoxelsLoader::loadCryptoVoxelsData(*world_state, client_thread, resource_manager);
+		conPrint("===================== Done Loading CryptoVoxels data. =====================");
+
+		while(1)
+		{
+			PlatformUtils::Sleep(10);
+
+			// Check messages back from queue
+			//conPrint("world_state->objects.size(): " + toString(world_state->objects.size()));
+		}
+		return 0;
 	}
-
-	// Send GetAllObjects msg
+	catch(glare::Exception& e)
 	{
-		SocketBufferOutStream packet(SocketBufferOutStream::DontUseNetworkByteOrder);
-		packet.writeUInt32(Protocol::GetAllObjects);
-		client_thread->enqueueDataToSend(packet);
+		stdErrPrint(e.what());
+		return 1;
 	}
-
-	// Wait until we have received all object data.
-	conPrint("Waiting for initial data to be received");
-	while(!client_thread->all_objects_received)
-	{
-		PlatformUtils::Sleep(20);
-		conPrintStr(".");
-	}
-
-	conPrint("Received objects.  world_state->objects.size(): " + toString(world_state->objects.size()));
-
-	conPrint("===================== Loading CryptoVoxels data =====================");
-	CryptoVoxelsLoader::loadCryptoVoxelsData(*world_state, client_thread, resource_manager);
-	conPrint("===================== Done Loading CryptoVoxels data. =====================");
-
-	while(1)
-	{
-		PlatformUtils::Sleep(10);
-
-		// Check messages back from queue
-		//conPrint("world_state->objects.size(): " + toString(world_state->objects.size()));
-	}
-	return 0;
 }
