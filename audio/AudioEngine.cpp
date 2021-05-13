f87339c1583be5ab55274269fa79299a4c67788f
Nicholas Chapman <nick@indigorenderer.com>
2021-05-13 01:46:26 +0000

    Added docs/licence.txt. Added Wav loading to AudioEngine, added playOneShotSound etc..
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@14670 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- audio/AudioEngine.cpp
+++ audio/AudioEngine.cpp
@@ -13,9 +13,32 @@ Copyright Glare Technologies Limited 2021 -
 #include <utils/ConPrint.h>
 #include <utils/StringUtils.h>
 #include <utils/PlatformUtils.h>
+#include <utils/MemMappedFile.h>
+#include <utils/FileInStream.h>
+
+
+
+float SoundFile::maxVal() const
+{
+	float m = -std::numeric_limits<float>::infinity();
+	for(float x : buf)
+		m = myMax(m, x);
+	return m;
+}
+
+
+float SoundFile::minVal() const
+{
+	float m = std::numeric_limits<float>::infinity();
+	for(float x : buf)
+		m = myMin(m, x);
+	return m;
+}
 
 
 AudioEngine::AudioEngine()
+:	audio(NULL),
+	resonance(NULL)
 {
 
 }
@@ -111,7 +134,7 @@ static int rtAudioCallback(void* output_buffer, void* input_buffer, unsigned int
 		}
 		else
 		{
-			conPrint("rtAudioCallback: not enough data in queue.");
+			//conPrint("rtAudioCallback: not enough data in queue.");
 			// Just write zeroes to output_buffer
 			for(unsigned int i=0; i<n_buffer_frames*2; ++i)
 				output_buffer_f[i] = 0.f;
@@ -151,15 +174,16 @@ public:
 					Lock lock(engine->mutex);
 
 					// Set resonance audio buffers for all audio sources
-					for(auto it = engine->audio_sources.begin(); it != engine->audio_sources.end(); ++it)
+					for(auto it = engine->audio_sources.begin(); it != engine->audio_sources.end(); )
 					{
 						AudioSource* source = it->ptr();
+						bool remove_source = false;
 
 						if(source->type == AudioSource::SourceType_Looping)
 						{
 							if(source->cur_read_i + frames_per_buffer <= source->buffer.size()) // If we can just copy the current buffer range directly from source->buffer:
 							{
-								const float* bufptr = &source->buffer[0]/*source->buffer.data()*/ + source->cur_read_i; // NOTE: bit of a hack here, assuming layout of circular buffer
+								const float* bufptr = &source->buffer[0]/*source->buffer.data()*/ + source->cur_read_i; // NOTE: bit of a hack here, assuming layout of circular buffer (e.g. no wrapping)
 								resonance->SetPlanarBuffer(source->resonance_handle, &bufptr, /*num channels=*/1, frames_per_buffer);
 
 								source->cur_read_i += frames_per_buffer;
@@ -179,6 +203,36 @@ public:
 								}
 
 								source->cur_read_i = cur_i;
+
+								const float* bufptr = buf;
+								resonance->SetPlanarBuffer(source->resonance_handle, &bufptr, /*num channels=*/1, frames_per_buffer);
+							}
+						}
+						if(source->type == AudioSource::SourceType_OneShot)
+						{
+							if(source->cur_read_i + frames_per_buffer <= source->buffer.size()) // If we can just copy the current buffer range directly from source->buffer:
+							{
+								const float* bufptr = &source->buffer[0]/*source->buffer.data()*/ + source->cur_read_i; // NOTE: bit of a hack here, assuming layout of circular buffer (e.g. no wrapping)
+								resonance->SetPlanarBuffer(source->resonance_handle, &bufptr, /*num channels=*/1, frames_per_buffer);
+
+								source->cur_read_i += frames_per_buffer;
+							}
+							else
+							{
+								// Copy data to a temporary contiguous buffer
+								size_t cur_i = source->cur_read_i;
+
+								for(size_t i=0; i<frames_per_buffer; ++i)
+								{
+									if(cur_i < source->buffer.size())
+										buf[i] = source->buffer[cur_i++];
+									else
+										buf[i] = 0;
+								}
+
+								source->cur_read_i = cur_i;
+
+								remove_source = source->remove_on_finish && (cur_i >= source->buffer.size()); // Remove the source if we reached the end of the buffer.
 
 								const float* bufptr = buf;
 								resonance->SetPlanarBuffer(source->resonance_handle, &bufptr, /*num channels=*/1, frames_per_buffer);
@@ -205,8 +259,13 @@ public:
 							}
 						}
 
-						//for(size_t i=0; i<frames_per_buffer; ++i)
-						//	buf[i] = 0.5;
+						if(remove_source)
+						{
+							resonance->DestroySource(source->resonance_handle);
+							it = engine->audio_sources.erase(it); // Remove currently iterated to source, advance iterator.
+						}
+						else
+							++it;
 					}
 
 					// Get mixed/filtered data from Resonance.
@@ -378,3 +437,270 @@ void AudioEngine::setHeadTransform(const Vec4f& head_pos, const Quatf& head_rot)
 	resonance->SetHeadPosition(head_pos[0], head_pos[1], head_pos[2]);
 	resonance->SetHeadRotation(head_rot.v[0], head_rot.v[1], head_rot.v[2], head_rot.v[3]);
 }
+
+
+inline int32 signExtend24BitValue(int32 x)
+{
+	if((x & 0x800000) != 0)
+	{
+		return x | 0xFF000000;
+	}
+	else
+	{
+		// Sign bit (left bit) was 0
+		return x;
+	}
+}
+
+
+// See http://soundfile.sapp.org/doc/WaveFormat/
+SoundFileRef AudioEngine::loadWavFile(const std::string& sound_file_path)
+{
+	try
+	{
+		SoundFileRef sound = new SoundFile();
+
+		FileInStream file(sound_file_path);
+
+		const uint32 riff_chunk_id = file.readUInt32();
+		if(riff_chunk_id != 0x46464952) // big endian: 0x52494646
+			throw glare::Exception("invalid header, expected RIFF");
+
+		const uint32 riff_chunk_size = file.readUInt32();
+		const uint32 riff_chunk_format = file.readUInt32();
+
+		const uint32 fmt_chunk_id = file.readUInt32();
+		if(fmt_chunk_id != 0x20746d66) // big endian: 0x666d7420
+			throw glare::Exception("invalid fmt chunk id, expected fmt");
+		const uint32 fmt_chunk_size = file.readUInt32();
+		const uint16 audio_format = file.readUInt16();
+		if(audio_format != 1)
+			throw glare::Exception("Unhandled audio format, only 1 (PCM) handled.");
+		const uint16 wav_num_channels = file.readUInt16();
+		const uint32 sample_rate = file.readUInt32();
+		const uint32 byte_rate = file.readUInt32();
+		const uint16 block_align = file.readUInt16();
+		const uint16 bits_per_sample = file.readUInt16();
+
+		sound->num_channels = 1; // mix down to mono
+		sound->sample_rate = sample_rate;
+
+		while(!file.endOfStream())
+		{
+			// Read chunk
+			const uint32 chunk_id = file.readUInt32();
+			const uint32 chunk_size = file.readUInt32();
+
+			if(chunk_id == 0x61746164) // "data", big endian: 0x64617461 
+			{
+				const uint32 bytes_per_sample = bits_per_sample / 8;
+
+				// Avoid divide by zeroes
+				if(wav_num_channels == 0)
+					throw glare::Exception("Invalid num channels");
+				if(bytes_per_sample == 0)
+					throw glare::Exception("Invalid bytes per sample");
+
+				// Read actual data, convert to float and resample to target sample rate
+				const uint32 num_samples = chunk_size / bytes_per_sample;
+
+				sound->buf.resize(num_samples / wav_num_channels); // TEMP: mix down to mono
+
+				const size_t file_bytes_remaining = file.fileSize() - file.getReadIndex();
+				const size_t expected_remaining = bytes_per_sample * num_samples;
+				if(file.getReadIndex() + expected_remaining > file.fileSize())
+					throw glare::Exception("not enough data in file.");
+
+				if(bytes_per_sample == 2)
+				{
+					if(wav_num_channels == 1)
+					{
+						std::vector<int16> temp(num_samples);
+						std::memcpy(temp.data(), file.currentReadPtr(), num_samples * bytes_per_sample); // Copy to ensure 16-bit aligned.
+
+						for(uint32 i=0; i<num_samples / wav_num_channels; ++i)
+							sound->buf[i] = temp[i] * (1.f / 32768);
+					}
+					else if(wav_num_channels == 2)
+					{
+						// TEMP: mix down to mono
+						std::vector<int16> temp(num_samples);
+						std::memcpy(temp.data(), file.currentReadPtr(), num_samples * bytes_per_sample); // Copy to ensure 16-bit aligned.
+
+						for(uint32 i=0; i<num_samples / wav_num_channels; ++i)
+						{
+							//const float left  = (int32)temp[i * 2 + 0] + (int32)temp[i * 2 + 1];
+							//const float right = temp[i * 2 + 1] * (1.f / 32768.f);
+							//sound->buf[i] = (left + right) * 0.5f; // TODO: optimise
+
+							// val = (left/32768.f + right/32768)*0.5 = ((left + right)/32768)*0.5 = left + right)/65536
+							sound->buf[i] = ((int32)temp[i * 2 + 0] + (int32)temp[i * 2 + 1])  * (1.f / 65536);
+						}
+					}
+				}
+				else if(bytes_per_sample == 3)
+				{
+					if(wav_num_channels == 1)
+					{
+						const uint8* src = (const uint8*)file.currentReadPtr();
+						for(uint32 i=0; i<num_samples; ++i)
+						{
+							int32 val;
+							std::memcpy(&val, &src[i * 3], 3);
+							sound->buf[i] = val * (1.f / 8388608);
+						}
+					}
+					else if(wav_num_channels == 2)
+					{
+						// TEMP: mix down to mono
+						const uint8* src = (const uint8*)file.currentReadPtr();
+						for(uint32 i=0; i<num_samples / wav_num_channels; ++i)
+						{
+							// NOTE: a much faster way to do this would be to handle blocks of 4 3-byte values, and use bitwise ops on them.
+							int32 left = 0;
+							int32 right = 0;
+							std::memcpy(&left, &src[i * 3 * 2 + 0], 3);
+							std::memcpy(&right, &src[i * 3 * 2 + 3], 3);
+							//sound->buf[i] = ((left + right) - 16777216) * (1.f / 16777216); // values seem to be in [0, 16777216), so map to [-8388608, 8388607)
+							sound->buf[i] = (signExtend24BitValue(left) + signExtend24BitValue(right)) * (1.f / 16777216); // values seem to be in [0, 16777216), so map to [-8388608, 8388607)
+						}
+					}
+				}
+				else if(bytes_per_sample == 4)
+				{
+					std::vector<int32> temp(num_samples);
+					std::memcpy(temp.data(), file.currentReadPtr(), num_samples * bytes_per_sample); // Copy to ensure 32-bit aligned.
+
+					for(uint32 i=0; i<num_samples; ++i)
+						sound->buf[i] = temp[i] * (1.f / 2147483648.f);
+				}
+				else
+					throw glare::Exception("Unhandled bytes_per_sample: " + toString(bytes_per_sample));
+
+				file.setReadIndex(file.getReadIndex() + expected_remaining);
+			}
+			else
+			{
+				// Unknown chunk, skip it
+				file.setReadIndex(file.getReadIndex() + chunk_size);
+			}
+		}
+
+		if(sound->buf.empty())
+			throw glare::Exception("Didn't find data chunk");
+
+		return sound;
+	}
+	catch(glare::Exception& e)
+	{
+		throw e;
+	}
+}
+
+
+SoundFileRef AudioEngine::loadSoundFile(const std::string& sound_file_path)
+{
+	if(hasExtension(sound_file_path, "wav"))
+	{
+		return loadWavFile(sound_file_path);
+	}
+	else
+		throw glare::Exception("Unhandled sound file extension: " + getExtension(sound_file_path));
+}
+
+
+void AudioEngine::playOneShotSound(const std::string& sound_file_path, const Vec4f& pos)
+{
+	SoundFileRef sound;
+	auto res = sound_files.find(sound_file_path);
+	if(res == sound_files.end())
+	{
+		// Load the sound
+		sound = loadSoundFile(sound_file_path);
+		sound_files.insert(std::make_pair(sound_file_path, sound));
+	}
+	else
+		sound = res->second;
+
+	// Make a new audio source
+	AudioSourceRef source = new AudioSource();
+	source->type = AudioSource::SourceType_OneShot;
+	source->buffer.pushBackNItems(sound->buf.data(), sound->buf.size());
+	source->remove_on_finish = true;
+
+	addSource(source);
+	resonance->SetSourcePosition(source->resonance_handle, pos[0], pos[1], pos[2]);
+}
+
+
+AudioSourceRef AudioEngine::addSourceFromSoundFile(const std::string& sound_file_path)
+{
+	SoundFileRef sound;
+	auto res = sound_files.find(sound_file_path);
+	if(res == sound_files.end())
+	{
+		// Load the sound
+		sound = loadSoundFile(sound_file_path);
+		sound_files.insert(std::make_pair(sound_file_path, sound));
+	}
+	else
+		sound = res->second;
+
+	// Make a new audio source
+	AudioSourceRef source = new AudioSource();
+	source->type = AudioSource::SourceType_OneShot;
+	source->remove_on_finish = false;
+	source->buffer.pushBackNItems(sound->buf.data(), sound->buf.size());
+	source->cur_read_i = source->buffer.size(); // Position read_i at end of buffer so doesn't play yet.
+
+	addSource(source);
+
+	return source;
+}
+
+
+#if BUILD_TESTS
+
+
+#include "MyThread.h"
+#include "ConPrint.h"
+#include "PlatformUtils.h"
+#include "Timer.h"
+#include "../utils/TestUtils.h"
+
+
+void AudioEngine::test()
+{
+	try
+	{
+		AudioEngine engine;
+		SoundFileRef sound;
+		/*sound = engine.loadWavFile("D:\\audio\\sound_effects\\171697__nenadsimic__menu-selection-click.wav"); // 24-bit
+		testAssert(sound->num_channels == 2);
+		testAssert(sound->sample_rate == 44100);
+		testAssert(sound->minVal() >= -1.f && sound->maxVal() <= 1.f);
+
+		sound = engine.loadWavFile("D:\\audio\\sound_effects\\366102__original-sound__confirmation-upward.wav"); // 16-bit
+		testAssert(sound->num_channels == 2);
+		testAssert(sound->sample_rate == 44100);
+		testAssert(sound->minVal() >= -1.f && sound->maxVal() <= 1.f);*/
+		
+		sound = engine.loadWavFile("D:\\audio\\sound_effects\\select_mono.wav"); // 16-bit mono
+		testAssert(sound->num_channels == 1);
+		testAssert(sound->sample_rate == 44100);
+		testAssert(sound->minVal() >= -1.f && sound->maxVal() <= 1.f);
+
+		sound = engine.loadWavFile("D:\\audio\\sound_effects\\462089__newagesoup__ethereal-woosh.wav"); // 16-bit stero
+		testAssert(sound->num_channels == 1); // mixed down to 1 channel
+		testAssert(sound->sample_rate == 44100);
+		testAssert(sound->minVal() >= -1.f && sound->maxVal() <= 1.f);
+	}
+	catch(glare::Exception& e)
+	{
+		failTest(e.what());
+	}
+}
+
+
+#endif // BUILD_TESTS
+
--- audio/AudioEngine.h
+++ audio/AudioEngine.h
@@ -15,6 +15,7 @@ Copyright Glare Technologies Limited 2021 -
 #include <utils/ThreadManager.h>
 #include <vector>
 #include <set>
+#include <map>
 
 
 class RtAudio;
@@ -32,13 +33,14 @@ public:
 		SourceType_Streaming
 	};
 
-	AudioSource() : cur_read_i(0), type(SourceType_Looping) {}
+	AudioSource() : cur_read_i(0), type(SourceType_Looping), remove_on_finish(true) {}
 	int resonance_handle;
 	//std::vector<float> buffer;
 	size_t cur_read_i;
 	CircularBuffer<float> buffer; // Read from front, enqueue to back.
 
 	SourceType type;
+	bool remove_on_finish; // for SourceType_OneShot
 };
 typedef Reference<AudioSource> AudioSourceRef;
 
@@ -62,6 +64,18 @@ struct AudioCallbackData
 };
 
 
+struct SoundFile : public ThreadSafeRefCounted
+{
+	float maxVal() const;
+	float minVal() const;
+
+	std::vector<float> buf;
+	uint32 num_channels;
+	uint32 sample_rate;
+};
+typedef Reference<SoundFile> SoundFileRef;
+
+
 /*=====================================================================
 AudioEngine
 -----------
@@ -77,17 +91,31 @@ public:
 
 	void addSource(AudioSourceRef source);
 
+	AudioSourceRef addSourceFromSoundFile(const std::string& sound_file_path);
+
 	void setSourcePosition(AudioSourceRef source, const Vec4f& pos);
 
 	void setHeadTransform(const Vec4f& head_pos, const Quatf& head_rot);
+
+	void playOneShotSound(const std::string& sound_file_path, const Vec4f& pos);
+
+	
+
+	static void test();
+private:
+	SoundFileRef loadWavFile(const std::string& sound_file_path);
+	SoundFileRef loadSoundFile(const std::string& sound_file_path);
 
 	RtAudio* audio;
 	vraudio::ResonanceAudioApi* resonance;
 
 	AudioCallbackData callback_data;
 
+public:
 	Mutex mutex; // Guards access to audio_sources, and resonance
 	std::set<AudioSourceRef> audio_sources;
 
 	ThreadManager resonance_thread_manager;
+
+	std::map<std::string, SoundFileRef> sound_files;
 };
