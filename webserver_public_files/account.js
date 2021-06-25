efc1b844c7f6349af6091c1fc96f756506da1c46
Nicholas Chapman <nick@indigorenderer.com>
2021-06-25 05:32:24 +0000

    Cleaned up account.js.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@14785 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- webserver_public_files/account.js
+++ webserver_public_files/account.js
@@ -2,13 +2,7 @@
 
 
 function accountConnectedSuccess(accounts) {
-    //alert("accountConnectedSuccess");
-
-    //console.log(accounts);
-
     var account = accounts[0];
-
-    //console.log(account);
 
     document.querySelector('.metamask-status-div').innerHTML = "Connected to account " + account;
 
@@ -19,14 +13,13 @@ function accountConnectedSuccess(accounts) {
     signButton.disabled = false;
 }
 
-function accountConnectedFailure() {
-   // alert("accountConnectedFailure");
 
+function accountConnectedFailure() {
     document.querySelector('.metamask-status-div').innerHTML = "Not connected";
 }
 
-function connectToMetaMaskAccount() {
 
+function connectToMetaMaskAccount() {
     try {
         var accounts = window.ethereum.request({ method: 'eth_requestAccounts' }).then(accountConnectedSuccess, accountConnectedFailure);
     }
@@ -36,24 +29,14 @@ function connectToMetaMaskAccount() {
 }
 
 
+// MetaMask (or similar) has succesfully signed a message
 function requestSignMessageSuccess(result) {
-   // alert("requestSignMessageSuccess");
-
-    console.log("result: " + result);
-
-    console.log("Making XMLHttpRequest...");
     const request = new XMLHttpRequest();
     const url = '/account_eth_sign_message_post?address=' + ethereum.selectedAddress + '&sig=' + result;
     request.open("POST", url);
     
-
-    console.log("XMLHttpRequest launched...");
-
     request.onload = function () {
         if (request.readyState === request.DONE && request.status === 200) {
-            //console.log(xhr.response, xhr.responseXML);
-
-            //alert("request.responseText: " + request.responseText);
 
             var response_json = JSON.parse(request.responseText);
 
@@ -62,7 +45,6 @@ function requestSignMessageSuccess(result) {
 
             if (msg != null)
                 alert(msg);
-           // alert("redirect_URL: " + redirect_URL);
 
             if (redirect_URL != null)
                 window.location = redirect_URL;
@@ -72,21 +54,18 @@ function requestSignMessageSuccess(result) {
     request.send();
 }
 
+
 function requestSignMessageFailure() {
-    //alert("requestSignMessageFailure");
 }
 
 
 function requestSignMessage() {
-    //alert("current_eth_signing_nonce: " + current_eth_signing_nonce);
     try {
         var accounts = window.ethereum.request({
             method: 'personal_sign', 'params': [ethereum.selectedAddress, "Please sign this message to confirm you own the Ethereum account.\n(Unique string: " + current_eth_signing_nonce + ")"]
-        }).
-            then(requestSignMessageSuccess, requestSignMessageFailure);
+        }).then(requestSignMessageSuccess, requestSignMessageFailure);
     }
     catch (error) {
         console.error("Error while signing message: " + error);
     }
 }
-
