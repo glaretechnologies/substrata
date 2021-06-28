8f113c6778fc6d6e71b13c9b7dcfc2aac7ec2480
Nicholas Chapman <nick@indigorenderer.com>
2021-06-28 02:36:05 +0000

    Cleaned up UInt256, cmake fix.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@14791 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- ethereum/UInt256.h
+++ ethereum/UInt256.h
@@ -8,28 +8,20 @@ Copyright Glare Technologies Limited 2021 -
 
 #include <Platform.h>
 #include <string>
-#include <vector>
 
 
-// See https://github.com/kvhnuke/Ethereum-Arduino/blob/master/Ethereum-Arduino/TX.h
-
-
+/*=====================================================================
+UInt256
+-------
+256-bit unsigned integer.
+Stored as an array of 32 bytes, in big endian order (most significant byte at data[0], least at data[31]).
+=====================================================================*/
 struct UInt256
 {
 	UInt256()
 	{
 		std::memset(data, 0, 32);
 	}
-
-	/*UInt256(uint32 x)
-	{
-		std::memset(data, 0, 32);
-		
-		std::memcpy(data + 28, (const uint8*)&x + 3, 1);
-		std::memcpy(data + 29, (const uint8*)&x + 2, 1);
-		std::memcpy(data + 30, (const uint8*)&x + 1, 1);
-		std::memcpy(data + 31, (const uint8*)&x + 0, 1);
-	}*/
 
 	UInt256(uint64 x)
 	{
@@ -45,15 +37,5 @@ struct UInt256
 		std::memcpy(data + 31, (const uint8*)&x + 0, 1);
 	}
 
-	std::string toDecimalString()
-	{
-		/*
-		x = data[0] * 2^(31*8) + data[1] * 2^(30*8) + ...  + data[30] * 2^(1*8) + data[31] * 2^0
-
-		*/
-		
-	}
-
 	uint8 data[32]; // big endian order
 };
-
