3cc0c6634057b4d33b957ad95006a1cf434b3ec9
Nicholas Chapman <nick@indigorenderer.com>
2021-11-14 10:22:23 +0000

    Fixed bug with parcel highlighting for some parcels on map.js Nudged in boundaries a bit to reduce overdraw. Tweaked fillOpacity for highlighted parcel.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@15234 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- webserver_public_files/map.js
+++ webserver_public_files/map.js
@@ -33,7 +33,7 @@ mymap.addLayer(L.gridLayer.gridDebug());*/
 
 for (var i = 0; i < poly_parcel_ids.length; i++) {
 
-    var is_highlighted_parcel = rect_parcel_ids[i] == highlight_parcel_id;
+    var is_highlighted_parcel = poly_parcel_ids[i] == highlight_parcel_id;
 
     var vert_i = i * 4;
     var polygon = L.polygon([
@@ -45,7 +45,7 @@ for (var i = 0; i < poly_parcel_ids.length; i++) {
         weight: 2, // line width in pixels
         color: (is_highlighted_parcel ? '#ef9518' : '#3388ff'),
         //fillColor: '#777',
-        fillOpacity: 0.05
+        fillOpacity: (is_highlighted_parcel ? 0.2 : 0.05)
     }).addTo(mymap);
 
     var popup = polygon.bindPopup("<a href=\"/parcel/" + poly_parcel_ids[i].toString() + "\">Parcel " + poly_parcel_ids[i].toString() + "</a>");
@@ -54,20 +54,23 @@ for (var i = 0; i < poly_parcel_ids.length; i++) {
         popup.openPopup();
 }
 
+
 for (var i = 0; i < rect_parcel_ids.length; i++) {
 
     var is_highlighted_parcel = rect_parcel_ids[i] == highlight_parcel_id;
 
+    let margin = 0.01
+
     var coord_i = i * 4;
     var polygon = L.rectangle(
-        [[rect_bound_coords[coord_i + 0], rect_bound_coords[coord_i + 1]], [rect_bound_coords[coord_i + 2], rect_bound_coords[coord_i + 3]]],
+        [[rect_bound_coords[coord_i + 0] + margin, rect_bound_coords[coord_i + 1] + margin], [rect_bound_coords[coord_i + 2] - margin, rect_bound_coords[coord_i + 3] - margin]],
         {
         weight: 2, // line width in pixels
         color: (is_highlighted_parcel ? '#ef9518' : '#3388ff'),
         //fillColor: '#777',
-        fillOpacity: 0.05
+        fillOpacity: (is_highlighted_parcel ? 0.2 : 0.05)
     }).addTo(mymap);
-    
+
     var popup = polygon.bindPopup("<a href=\"/parcel/" + rect_parcel_ids[i].toString() + "\">Parcel " + rect_parcel_ids[i].toString() + "</a>");
 
     if (is_highlighted_parcel)
