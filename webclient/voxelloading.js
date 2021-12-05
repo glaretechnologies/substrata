aebd03a760dff09435e7a8f3fc58968eaacf0536
Nicholas Chapman <nick@indigorenderer.com>
2021-12-05 13:33:58 +0000

    Cleaned up voxelloading.js.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@15318 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- webclient/voxelloading.js
+++ webclient/voxelloading.js
@@ -10,25 +10,14 @@ import * as THREE from './build/three.module.js';
 
 function decompressVoxels(compressed_voxels) {
 	let decompressed_voxels_uint8 = fzstd.decompress(new Uint8Array(compressed_voxels));
-    //console.log("decompressed_voxels_uint8:");
-    //console.log(decompressed_voxels_uint8);
 
     // A voxel is
     //Vec3<int> pos;
 	//int mat_index; // Index into materials
-    //let voxel_ints = new Uint32Array(decompressed_voxels))
-
-	//console.log("decompressed_voxels_uint8.buffer:")
-	//console.log(decompressed_voxels_uint8.buffer)
 
     let voxel_data = new Int32Array(decompressed_voxels_uint8.buffer, decompressed_voxels_uint8.byteOffset, decompressed_voxels_uint8.byteLength / 4);
 
-	//console.log("voxel_data:");
-   // console.log(voxel_data);
-
     let voxels_out = []
-
-    
 
     let cur_x = 0;
     let cur_y = 0;
@@ -36,7 +25,6 @@ function decompressVoxels(compressed_voxels) {
 
     let read_i = 0;
 	let num_mats = voxel_data[read_i++];
-	//console.log("num_mats: " + num_mats)
     if(num_mats > 2000)
         throw "Too many voxel materials";
 	        
@@ -46,18 +34,15 @@ function decompressVoxels(compressed_voxels) {
 		//console.log("mat: " + m + ", count: " + count)
 		for(let i=0; i<count; ++i)
 		{
-			//Vec3<int> relative_pos;
-			// instream.readData(&relative_pos, sizeof(Vec3<int>));
             let rel_x = voxel_data[read_i++];
             let rel_y = voxel_data[read_i++];
             let rel_z = voxel_data[read_i++];
 
-			//const Vec3<int> pos = current_pos + relative_pos;
+			//pos = current_pos + relative_pos;
             let v_x = cur_x + rel_x;
             let v_y = cur_y + rel_y;
             let v_z = cur_z + rel_z;
 
-			//group_out.voxels.push_back(Voxel(pos, m));
             voxels_out.push(v_x);
             voxels_out.push(v_y);
             voxels_out.push(v_z);
@@ -111,29 +96,6 @@ function doMakeMeshForVoxels(voxels, num_mats)
 		bounds[mat_i * 6 + 5] = Math.max(bounds[mat_i * 6 + 5], v_z);
 	}
 
-	/*let largest_vol = 0;
-	for(let i=0; i<num_mats; ++i) {
-		if(bounds[i * 6] == 1000000000)
-			continue; // No voxels for this mat.
-
-		let min_x = bounds[i*6 + 0];
-		let min_y = bounds[i*6 + 1];
-		let min_z = bounds[i*6 + 2];
-		let max_x = bounds[i*6 + 3];
-		let max_y = bounds[i*6 + 4];
-		let max_z = bounds[i*6 + 5];
-
-		let span_x = max_x - min_x;
-		let span_y = max_y - min_y;
-		let span_z = max_z - min_z;
-
-		let vol = span_x * span_y * span_z;
-		largest_vol = Math.max(vol, largest_vol);
-	}*/
-
-	//console.log("bounds:")
-	//console.log(bounds)
-
 	// Get overall min and max coords
 	let min_x = 1000000000;
 	let min_y = 1000000000;
@@ -180,9 +142,6 @@ function doMakeMeshForVoxels(voxels, num_mats)
 
 		voxel_grid[(v_z - min_z) * (span_x * span_y) + (v_y - min_y) * span_x + (v_x - min_x)] = mat_i;
 	}
-
-	//console.log("voxel_grid:");
-	//console.log(voxel_grid);
 
 	let geometry = new THREE.BufferGeometry();
 	let verts = []
@@ -240,17 +199,8 @@ function doMakeMeshForVoxels(voxels, num_mats)
 			//console.log("dim_end: " + dim_end);
 
 			// Make a map to indicate processed voxel faces.  Processed = included in a greedy quad already.
-			//Array2D<bool> face_needed(a_end - a_min, b_end - b_min);
 			let face_needed_x_span = (a_end - a_min);
 			let face_needed = new Uint8Array(face_needed_x_span * (b_end - b_min));
-			//console.log("*********************************");
-			//console.log("face_needed: ");
-			//console.log(face_needed);
-			//face_needed[0] = 123;
-			//console.log("face_needed: ");
-			//console.log(face_needed);
-			//console.log("face_needed[0]: ");
-			//console.log(face_needed[0]);
 
 
 			for(let dim_coord = dim_min; dim_coord < dim_end; ++dim_coord)
@@ -267,19 +217,13 @@ function doMakeMeshForVoxels(voxels, num_mats)
 					vox[dim_a] = x;
 					vox[dim_b] = y;
 
-					//console.log("vox: ")
-					//console.log(vox)
-
 					let this_face_needed = 0;
 					let grid_val = voxel_grid[(vox[2] - min_z) * (span_x * span_y) + (vox[1] - min_y) * span_x + (vox[0] - min_x)];
-					//console.log("grid_val: " + grid_val)
 					if(grid_val == mat_i) // If there is a voxel here with mat_i
 					{
 						adjacent_vox_pos[dim_a] = x;
 						adjacent_vox_pos[dim_b] = y;
 
-						//console.log("dim_coord: " + dim_coord)
-						//console.log("dim_min: " + dim_min)
 						if(dim_coord == dim_min) // If adjacent coords are out of voxel grid:
 							this_face_needed = 1;
 						else {
@@ -288,23 +232,14 @@ function doMakeMeshForVoxels(voxels, num_mats)
 								this_face_needed = 1;
 						}
 					}
-					//face_needed.elemthis_face_neededx - a_min, y - b_min) = this_face_needed;
-					//console.log("this_face_needed: ");
-					//console.log(this_face_needed);
 					face_needed[(y - b_min) * face_needed_x_span + (x - a_min)] = this_face_needed;
 				}
-
-				//console.log("face_needed: ")
-				//console.log(face_needed)
-				//console.log("face_needed[0]: ")
-				//console.log(face_needed[0])
 
 				// For each voxel face:
 				for(let start_y=b_min; start_y<b_end; ++start_y)
 				for(let start_x=a_min; start_x<a_end; ++start_x) {
 				
-					//if(face_needed.elem(start_x - a_min, start_y - b_min)) // If we need a face here:
-					if(face_needed[(start_y - b_min) * face_needed_x_span + (start_x - a_min)]) {
+					if (face_needed[(start_y - b_min) * face_needed_x_span + (start_x - a_min)]) { // If we need a face here:
 					
 						// Start a quad here (start corner at (start_x, start_y))
 						// The quad will range from (start_x, start_y) to (end_x, end_y)
@@ -322,7 +257,7 @@ function doMakeMeshForVoxels(voxels, num_mats)
 								{
 									// Check y values for new x = end_x
 									for(let y = start_y; y < end_y; ++y)
-										if(!face_needed[(y - b_min) * face_needed_x_span + (end_x - a_min)]/*face_needed.elem(end_x - a_min, y - b_min)*/)
+										if(!face_needed[(y - b_min) * face_needed_x_span + (end_x - a_min)])
 										{
 											x_increase_ok = false;
 											break;
@@ -342,7 +277,7 @@ function doMakeMeshForVoxels(voxels, num_mats)
 								{
 									// Check x values for new y = end_y
 									for(let x = start_x; x < end_x; ++x)
-										if(!face_needed[(end_y - b_min) * face_needed_x_span + (x - a_min)]/*face_needed.elem(x - a_min, end_y - b_min)*/)
+										if(!face_needed[(end_y - b_min) * face_needed_x_span + (x - a_min)])
 										{
 											y_increase_ok = false;
 											break;
@@ -359,7 +294,6 @@ function doMakeMeshForVoxels(voxels, num_mats)
 						// We have worked out the greedy quad.  Mark elements in it as processed
 						for(let y=start_y; y < end_y; ++y)
 						for(let x=start_x; x < end_x; ++x)
-							//face_needed.elem(x - a_min, y - b_min) = false;
 							face_needed[(y - b_min) * face_needed_x_span + (x - a_min)] = 0;
 
 						// Add the greedy quad
@@ -455,12 +389,8 @@ function doMakeMeshForVoxels(voxels, num_mats)
 								this_face_needed = 1;
 						}
 					}
-					//face_needed.elem(x - a_min, y - b_min) = this_face_needed;
 					face_needed[(y - b_min) * face_needed_x_span + (x - a_min)] = this_face_needed;
 				}
-
-				//console.log("face_needed[0]: ");
-				//console.log(face_needed[0]);
 
 				// For each voxel face:
 				for(let start_y=b_min; start_y<b_end; ++start_y)
@@ -484,7 +414,7 @@ function doMakeMeshForVoxels(voxels, num_mats)
 								{
 									// Check y values for new x = end_x
 									for(let y = start_y; y < end_y; ++y)
-										if(!face_needed[(y - b_min) * face_needed_x_span + (end_x - a_min)]/*face_needed.elem(end_x - a_min, y - b_min)*/)
+										if(!face_needed[(y - b_min) * face_needed_x_span + (end_x - a_min)])
 										{
 											x_increase_ok = false;
 											break;
@@ -504,7 +434,7 @@ function doMakeMeshForVoxels(voxels, num_mats)
 								{
 									// Check x values for new y = end_y
 									for(let x = start_x; x < end_x; ++x)
-										if(!face_needed[(end_y - b_min) * face_needed_x_span + (x - a_min)]/*face_needed.elem(x - a_min, end_y - b_min)*/)
+										if(!face_needed[(end_y - b_min) * face_needed_x_span + (x - a_min)])
 										{
 											y_increase_ok = false;
 											break;
@@ -522,12 +452,10 @@ function doMakeMeshForVoxels(voxels, num_mats)
 						for(let y=start_y; y < end_y; ++y)
 						for(let x=start_x; x < end_x; ++x)
 							face_needed[(y - b_min) * face_needed_x_span + (x - a_min)] = 0;
-							//face_needed.elem(x - a_min, y - b_min) = false;
 
 						let quad_dim_coord = dim_coord + 1.0;
 
 						// Add the greedy quad
-						//unsigned int v_i[4];
 						let v_i = []
 						let v = new Float32Array(3);
 						v[dim] = quad_dim_coord;
@@ -592,12 +520,10 @@ function doMakeMeshForVoxels(voxels, num_mats)
 		geometry.addGroup(/*start index=*/mat_vert_indices_start, /*count=*/tri_vert_indices.length - mat_vert_indices_start, mat_i);
 	} // End For each mat
 
-	
 	geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(verts), 3));
 	geometry.setIndex(tri_vert_indices);
 	return geometry;
 }
-
 
 
 // compressed_voxels is an ArrayBuffer 
@@ -619,7 +545,6 @@ export function makeMeshForVoxelGroup(compressed_voxels, model_lod_level) {
 		subsample_factor = 2;
 	else if (use_model_lod_level == 2)
 		subsample_factor = 4;
-
 
 	//console.log("makeMeshForVoxelGroup");
 	//console.log("num_voxels: " + num_voxels);
