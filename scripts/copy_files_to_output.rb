019d707bfe12435aa262dbb17d93aab88c5553ac
Nicholas Chapman <nick@indigorenderer.com>
2022-04-28 02:20:10 +0000

    improved for mac
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@15682 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- scripts/copy_files_to_output.rb
+++ scripts/copy_files_to_output.rb
@@ -5,7 +5,7 @@
 require './dist_utils.rb'
 
 
-def copy_files(vs_version, bitness)
+def copy_files(vs_version)
 
 	begin
 		output_dir = getCmakeBuildDir(vs_version, "Debug")
@@ -22,6 +22,17 @@ end
 
 
 if OS.windows?
-	copy_files(2019, 64)
-elsif OS.linux?
+	copy_files(2019)
+else
+	begin
+		output_dir = getCmakeBuildDir(0, "Debug") + "/gui_client.app/Contents/MacOS/../Resources"
+
+		copyCyberspaceResources(output_dir)
+	end
+
+	begin
+		#output_dir = getCmakeBuildDir(vs_version, "RelWithDebInfo")
+
+		#copyCyberspaceResources(output_dir)
+	end
 end
