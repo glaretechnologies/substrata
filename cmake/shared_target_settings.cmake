9b5a11aed029d302934153a8b4073f0cf08737c7
Nicholas Chapman <nick@indigorenderer.com>
2022-05-29 07:08:45 +0000

    refactoring cmake
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@15792 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- cmake/shared_target_settings.cmake
+++ cmake/shared_target_settings.cmake
@@ -10,25 +10,37 @@ MESSAGE("Current target is: " ${CURRENT_TARGET})
 
 
 if(WIN32)
-	# add AuxFunctions as a dependency
-	#add_dependencies(${CURRENT_TARGET} AuxFunctions)
-
+	
 	set_target_properties(${CURRENT_TARGET} PROPERTIES LINK_FLAGS "")
 
 	# /DEBUG /OPT:REF /OPT:ICF are for writing pdb files that can be used with minidumps.
 	set_target_properties(${CURRENT_TARGET} PROPERTIES LINK_FLAGS_RELEASE "/DEBUG /OPT:REF /OPT:ICF /LTCG")
 
-	SET(INDIGO_WIN32_LIBS odbc32
+	target_link_libraries(${CURRENT_TARGET}
+		indigo_libs
+		odbc32
 		comctl32
 		rpcrt4
 		Iphlpapi  # For GetAdaptersInfo() in SystemInfo::getMACAddresses().
 		ws2_32 # Winsock
 		delayimp # for delay loading via command line, as 2008 generator doesn't seem to support the VS setting for it
-		)
+		
+		debug     "${jpegturbodir}-debug/lib/turbojpeg-static.lib"
+		optimized "${jpegturbodir}/lib/turbojpeg-static.lib"
+	)
+	
 elseif(APPLE)
-	# NOTE: -stdlib=libc++ is needed for C++11.
-	set_target_properties(${CURRENT_TARGET} PROPERTIES LINK_FLAGS "-std=c++11 -stdlib=libc++ -dead_strip -F/Library/Frameworks -framework CoreServices")
+	
+	# NOTE: -stdlib=libc++ is needed for C++11 etc.
+	set_target_properties(${CURRENT_TARGET} PROPERTIES LINK_FLAGS "-std=c++14 -stdlib=libc++ -dead_strip -F/Library/Frameworks -framework CoreServices")
+	
+	target_link_libraries(${CURRENT_TARGET}
+		indigo_libs
+		${jpegturbodir}/lib/libjpeg.a
+	)
+	
 else()
+	
 	# Linux
 	# Add required Sanitizer link flags
 	if(NOT INDIGO_USE_SANITIZER STREQUAL "")
@@ -38,33 +50,11 @@ else()
 	# Note that for some stupid reason, -no-pie is needed to get the executable to show up as clickable in the Ubuntu files app.
 	# See https://askubuntu.com/questions/1056882/i-cannot-run-any-executable-from-nautilus and https://gitlab.gnome.org/GNOME/nautilus/-/issues/1601
 	# Note that the server target doesn't include this cmake file, so the -no-pie won't apply to it.
-	set_target_properties(${CURRENT_TARGET} PROPERTIES LINK_FLAGS     "${SANITIZER_LINK_FLAGS} -Xlinker -rpath='$ORIGIN/lib' -no-pie")
-endif()
-
-
-#if(WIN32)
-#	SET(INDIGO_EMBREE_LIB) # On Windows, embree is built as a dll that is loaded at runtime.
-#else()
-#	SET(INDIGO_EMBREE_LIB embree) # Linux and OSX need to link against the static embree lib.
-#endif()
-
-
-if(WIN32)
+	set_target_properties(${CURRENT_TARGET} PROPERTIES LINK_FLAGS     "-std=c++14 ${SANITIZER_LINK_FLAGS} -Xlinker -rpath='$ORIGIN/lib' -no-pie")
+	
 	target_link_libraries(${CURRENT_TARGET}
-		debug     "${jpegturbodir}-debug/lib/turbojpeg-static.lib"
-		optimized "${jpegturbodir}/lib/turbojpeg-static.lib"
-	)
-else()
-	target_link_libraries(${CURRENT_TARGET}
+		indigo_libs
 		${jpegturbodir}/lib/libjpeg.a
 	)
+
 endif()
-
-
-target_link_libraries(${CURRENT_TARGET}
-indigo_libs
-${INDIGO_WIN32_LIBS}
-${LINUX_LIBS}
-#C:/programming/indigo/output/vs2019/indigo_x64/Debug/indigo_sdk_lib.lib
-#C:/programming/indigo/output/vs2019/indigo_x64/RelWithDebInfo/indigo_sdk_lib.lib
-)
