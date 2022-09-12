f61ec3c2bb4564bb5b98e2a2c9914c9848c6eb1d
Nicholas Chapman <nick@indigorenderer.com>
2022-09-12 05:16:49 +0000

    Cleaned up cmake files a bit.  Removed some unused file cmake includes.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@16171 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- cmake/shared_cxx_settings.cmake
+++ cmake/shared_cxx_settings.cmake
@@ -12,7 +12,6 @@ macro(addIncludeDirectory dir_to_include)
 endmacro(addIncludeDirectory)
 
 
-addIncludeDirectory("${fftssdir}/include")
 addIncludeDirectory(${jpegturbodir}/include)
 #addIncludeDirectory(${jpegturbodir}) # This one works on linux/mac
 addIncludeDirectory(${pngdir})
@@ -41,18 +40,11 @@ else()
 endif()
 
 
-addIncludeDirectory("${luadir}/src")
 addIncludeDirectory("${GLARE_CORE_TRUNK_DIR_ENV}/")
 addIncludeDirectory("${GLARE_CORE_TRUNK_DIR_ENV}/utils")
 addIncludeDirectory("${GLARE_CORE_TRUNK_DIR_ENV}/networking")
 addIncludeDirectory("${GLARE_CORE_TRUNK_DIR_ENV}/maths")
-#addIncludeDirectory("${CMAKE_SOURCE_DIR}/embree/common")
-#addIncludeDirectory("${CMAKE_SOURCE_DIR}/embree/rtcore")
 addIncludeDirectory("${GLARE_CORE_TRUNK_DIR_ENV}/opengl") # For Glew
-addIncludeDirectory("${sparsehashdir}/src")
-if(WIN32)
-addIncludeDirectory("${sparsehashdir}/src/windows")
-endif()
 addIncludeDirectory("${GLARE_CORE_TRUNK_DIR_ENV}/giflib/lib")
 addIncludeDirectory("${GLARE_CORE_TRUNK_DIR_ENV}/little_cms/include")
 addIncludeDirectory("${zstddir}/lib")
@@ -69,9 +61,6 @@ addIncludeDirectory("${GLARE_CORE_TRUNK_DIR_ENV}/opencl/khronos")
 addIncludeDirectory("${INDIGO_LIBS_ENV}/BugSplat/inc")
 
 
-#addIncludeDirectory("${MYSQL_CONNECTOR_DIR}/include")
-
-
 # Append INDIGO_SHARED_INCLUDE_DIRS
 
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${INDIGO_SHARED_INCLUDE_DIRS}")
@@ -80,9 +69,9 @@ SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${INDIGO_SHARED_INCLUDE_DIRS}")
 
 # some non windows preprocessor defs
 if(WIN32)
-        add_definitions(-DPLATFORM_WINDOWS)
+	add_definitions(-DPLATFORM_WINDOWS)
 else()
-        add_definitions(-DCOMPILER_GCC -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS) # LLVM wants this to be defined.
+	add_definitions(-DCOMPILER_GCC -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS) # LLVM wants this to be defined.
 endif()
 
 add_definitions(-DNO_EMBREE)
@@ -133,14 +122,15 @@ if(WIN32)
 	
 	################################
 	
-	add_definitions(/GS- /fp:fast)
+	add_definitions(/GS-) # Disable Security Check (/GS-)
+	add_definitions(/fp:fast) # Set fast floating point model.
 	
 	add_definitions(-D__SSE4_1__)
 	
 	add_definitions(/std:c++17)
 	add_definitions(/Zc:__cplusplus) # Qt wants this for some reason
 	
-	add_definitions(-DPERFORMANCEAPI_ENABLED=${PERFORMANCEAPI_ENABLED})
+	add_definitions(-DPERFORMANCEAPI_ENABLED=${PERFORMANCEAPI_ENABLED}) # Turn on or off superluminal profiler integration.
 	
 	# Consider some options.
 	if(NO_WHOLE_PROGRAM_OPT)
@@ -149,19 +139,10 @@ if(WIN32)
 		SET(GL_OPT "/GL")
 	endif()
 	
-	if(WIN_RUNTIME_STATIC)
-		SET(WIN_RUNTIME_OPT "/MT")
-		SET(WIN_RUNTIME_OPT_DEBUG "/MTd")
-	else()
-		SET(WIN_RUNTIME_OPT)
-		SET(WIN_RUNTIME_OPT_DEBUG)
-	endif()
-	
 	# Append optimisation flags.
-	SET(CMAKE_CXX_FLAGS_DEBUG			"${CMAKE_CXX_FLAGS_DEBUG}			${WIN_RUNTIME_OPT_DEBUG} ")
-	SET(CMAKE_CXX_FLAGS_SDKDEBUG		"${CMAKE_CXX_FLAGS_SDKDEBUG}		${WIN_RUNTIME_OPT_DEBUG} -DNDEBUG")
-	SET(CMAKE_CXX_FLAGS_RELEASE			"${CMAKE_CXX_FLAGS_RELEASE}			${WIN_RUNTIME_OPT} -D_SECURE_SCL=0 /O2 ${GL_OPT} -DNDEBUG /Zi")
-	SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO	"${CMAKE_CXX_FLAGS_RELWITHDEBINFO}	${WIN_RUNTIME_OPT} /O2 -D_SECURE_SCL=0 -DNDEBUG ")
+	SET(CMAKE_CXX_FLAGS_DEBUG			"${CMAKE_CXX_FLAGS_DEBUG}			")
+	SET(CMAKE_CXX_FLAGS_RELEASE			"${CMAKE_CXX_FLAGS_RELEASE}			-D_SECURE_SCL=0 /O2 -DNDEBUG ${GL_OPT} /Zi") # /Zi = use program database for debug info (.pdb)
+	SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO	"${CMAKE_CXX_FLAGS_RELWITHDEBINFO}	-D_SECURE_SCL=0 /O2 -DNDEBUG")
 	
 elseif(APPLE)
 	add_definitions(-DOSX -DINDIGO_NO_OPENMP)
@@ -177,7 +158,6 @@ elseif(APPLE)
 	SET(APPLE_C_CXX_OPTIONS "-stdlib=libc++ -Wall -fPIC -mmmx -msse -msse2 -mssse3 -msse4.1 -arch x86_64 -Wthread-safety")
 	
 	SET(COMMON_C_CXX_OPTIONS_DEBUG				"${APPLE_C_CXX_OPTIONS} -gdwarf-2")
-	SET(COMMON_C_CXX_OPTIONS_SDKDEBUG			"${APPLE_C_CXX_OPTIONS} -gdwarf-2 -DNDEBUG")
 	SET(COMMON_C_CXX_OPTIONS_RELEASE			"${APPLE_C_CXX_OPTIONS} -gdwarf-2 -O3 -DNDEBUG") # NOTE: removed -fvisibility=hidden to allow debug symbols in exe.
 	# For some reason tests will fail with -O2 so we build with settigns similar to release
 	#SET(COMMON_C_CXX_OPTIONS_RELWITHDEBINFO	"${APPLE_C_CXX_OPTIONS} -O2 -DNDEBUG")
@@ -187,12 +167,10 @@ elseif(APPLE)
 	# NOTE: -Wno-reorder gets rid of warnings like: warning: 'IndigoDriver::appdata_path' will be initialized after 'std::string IndigoDriver::scenefilepath'.
 	# NOTE: c++14 seems to be needed by CEF
 	SET(CMAKE_CXX_FLAGS_DEBUG			"${CMAKE_CXX_FLAGS_DEBUG}			${COMMON_C_CXX_OPTIONS_DEBUG} -std=c++14 -Wno-reorder")
-	SET(CMAKE_CXX_FLAGS_SDKDEBUG		"${CMAKE_CXX_FLAGS_SDKDEBUG}		${COMMON_C_CXX_OPTIONS_SDKDEBUG} -std=c++14 -Wno-reorder")
 	SET(CMAKE_CXX_FLAGS_RELEASE			"${CMAKE_CXX_FLAGS_RELEASE}			${COMMON_C_CXX_OPTIONS_RELEASE} -std=c++14 -fvisibility-inlines-hidden -Wno-reorder")
 	SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO	"${CMAKE_CXX_FLAGS_RELWITHDEBINFO}	${COMMON_C_CXX_OPTIONS_RELWITHDEBINFO} -std=c++14 -fvisibility-inlines-hidden -Wno-reorder")
 	
 	SET(CMAKE_C_FLAGS_DEBUG				"${CMAKE_C_FLAGS_DEBUG}				${COMMON_C_CXX_OPTIONS_DEBUG}")
-	SET(CMAKE_C_FLAGS_SDKDEBUG			"${CMAKE_C_FLAGS_SDKDEBUG}			${COMMON_C_CXX_OPTIONS_SDKDEBUG}")
 	SET(CMAKE_C_FLAGS_RELEASE			"${CMAKE_C_FLAGS_RELEASE}			${COMMON_C_CXX_OPTIONS_RELEASE}")
 	SET(CMAKE_C_FLAGS_RELWITHDEBINFO	"${CMAKE_C_FLAGS_RELWITHDEBINFO}	${COMMON_C_CXX_OPTIONS_RELWITHDEBINFO}")
 
@@ -208,33 +186,22 @@ else() # Linux
 		SET(LINUX_C_CXX_OPTIONS "${LINUX_C_CXX_OPTIONS} -fsanitize=${USE_SANITIZER} -fno-omit-frame-pointer -g") # -fPIE
 	endif()
 
-
-	# Turn on LCOV (code coverage measurement) support if requested.  See http://ltp.sourceforge.net/coverage/lcov.php
-	if(INDIGO_USE_LCOV)
-		# See http://gcc.gnu.org/onlinedocs/gcc/Debugging-Options.html
-		SET(LINUX_C_CXX_OPTIONS "${LINUX_C_CXX_OPTIONS} --coverage -g")
-	endif()
-		
 	SET(COMMON_C_CXX_OPTIONS_DEBUG				"${LINUX_C_CXX_OPTIONS} -g")
-	SET(COMMON_C_CXX_OPTIONS_SDKDEBUG			"${LINUX_C_CXX_OPTIONS} -g -DNDEBUG")
 	SET(COMMON_C_CXX_OPTIONS_RELEASE			"${LINUX_C_CXX_OPTIONS} -O3 -g -DNDEBUG")
 	SET(COMMON_C_CXX_OPTIONS_RELWITHDEBINFO		"${LINUX_C_CXX_OPTIONS} -O2 -g -DNDEBUG")
 
 	# Append optimisation flags.
 	SET(CMAKE_CXX_FLAGS_DEBUG			"${CMAKE_CXX_FLAGS_DEBUG}			${COMMON_C_CXX_OPTIONS_DEBUG} -std=c++14 -Wno-reorder")
-	SET(CMAKE_CXX_FLAGS_SDKDEBUG		"${CMAKE_CXX_FLAGS_SDKDEBUG}		${COMMON_C_CXX_OPTIONS_SDKDEBUG} -std=c++14 -Wno-reorder")
 	SET(CMAKE_CXX_FLAGS_RELEASE			"${CMAKE_CXX_FLAGS_RELEASE}			${COMMON_C_CXX_OPTIONS_RELEASE} -std=c++14 -Wno-reorder")
 	SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO	"${CMAKE_CXX_FLAGS_RELWITHDEBINFO}	${COMMON_C_CXX_OPTIONS_RELWITHDEBINFO} -std=c++14 -Wno-reorder")
 	
 	SET(CMAKE_C_FLAGS_DEBUG				"${CMAKE_C_FLAGS_DEBUG}				${COMMON_C_CXX_OPTIONS_DEBUG}")
-	SET(CMAKE_C_FLAGS_SDKDEBUG			"${CMAKE_C_FLAGS_SDKDEBUG}			${COMMON_C_CXX_OPTIONS_SDKDEBUG}")
 	SET(CMAKE_C_FLAGS_RELEASE			"${CMAKE_C_FLAGS_RELEASE}			${COMMON_C_CXX_OPTIONS_RELEASE}")
 	SET(CMAKE_C_FLAGS_RELWITHDEBINFO	"${CMAKE_C_FLAGS_RELWITHDEBINFO}	${COMMON_C_CXX_OPTIONS_RELWITHDEBINFO}")
 endif()
 
 
 # Append BUILD_TEST=1 preprocessor def for debug + release with debug info, all platforms.
-# Note: SDKDebug should have BUILD_TEST off.
 SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO	"${CMAKE_CXX_FLAGS_RELWITHDEBINFO}	-DBUILD_TESTS=1")
 SET(CMAKE_CXX_FLAGS_DEBUG			"${CMAKE_CXX_FLAGS_DEBUG}			-DBUILD_TESTS=1")
 
@@ -244,11 +211,3 @@ SET(CMAKE_C_FLAGS_DEBUG				"${CMAKE_C_FLAGS_DEBUG}				-DBUILD_TESTS=1")
 
 # Add general preprocessor definitions.
 add_definitions(-DOPENEXR_SUPPORT -DPNG_ALLOW_BENIGN_ERRORS -DPNG_INTEL_SSE -DPNG_NO_SETJMP)
-
-if(INDIGO_USE_OPENCL)
-	add_definitions(-DUSE_OPENCL=1)
-else()
-	add_definitions(-DUSE_OPENCL=0)
-endif()
-
-
