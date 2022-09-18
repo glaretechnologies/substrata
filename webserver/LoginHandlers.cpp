2ee6fa03a029cc51ba5b7b54e6f563f8d3244660
Nicholas Chapman <nick@indigorenderer.com>
2022-09-18 15:30:10 +0000

    Jazzed up LoginHandlers forms with label tags and form-field divs.
    git-svn-id: file:///home/nick/svnrepo/repo/new_cyberspace/trunk@16191 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- webserver/LoginHandlers.cpp
+++ webserver/LoginHandlers.cpp
@@ -135,9 +135,18 @@ void renderLoginPage(const web::RequestInfo& request_info, web::ReplyInfo& reply
 		page_out += "<div class=\"msg\" style=\"background-color: yellow\">" + msg.HTMLEscaped() + "</div>  \n";
 
 	page_out += "<form action=\"login_post\" method=\"post\">";
-	page_out += "<input type=\"hidden\" name=\"return\" value=\"" + web::Escaping::HTMLEscape(request_info.getURLParam("return").str()) + "\"><br>";
-	page_out += "username: <input type=\"text\" name=\"username\"><br>";
-	page_out += "password: <input type=\"password\" name=\"password\"><br/>";
+	page_out += "<input type=\"hidden\" name=\"return\" value=\"" + web::Escaping::HTMLEscape(request_info.getURLParam("return").str()) + "\"><br/>";
+
+	page_out += "<div class=\"form-field\">";
+	page_out += "<label for=\"username\">username</label><br/>";
+	page_out += "<input id=\"username\"			autocomplete=\"username\"			type=\"text\"		name=\"username\">";
+	page_out += "</div>";
+
+	page_out += "<div class=\"form-field\">";
+	page_out += "<label for=\"current-password\">password</label><br/>";
+	page_out += "<input id=\"current-password\"	autocomplete=\"current-password\"	type=\"password\"	name=\"password\">"; // See https://web.dev/sign-in-form-best-practices/#current-password
+	page_out += "</div>";
+
 	page_out += "<input type=\"submit\" value=\"Log in\">";
 	page_out += "</form>";
 
@@ -271,9 +280,22 @@ void renderSignUpPage(ServerAllWorldsState& world_state, const web::RequestInfo&
 
 		page_out += "<form action=\"signup_post\" method=\"post\">";
 		page_out += "<input type=\"hidden\" name=\"return\" value=\"" + web::Escaping::HTMLEscape(request_info.getURLParam("return").str()) + "\"><br>";
-		page_out += "username: <input type=\"text\" name=\"username\"><br>";
-		page_out += "email:    <input type=\"email\" name=\"email\"><br>";
-		page_out += "password: <input type=\"password\" name=\"password\"><br/>";
+
+		page_out += "<div class=\"form-field\">";
+		page_out += "<label for=\"username\">username</label><br/>";
+		page_out += "<input id=\"username\"		autocomplete=\"username\"		type=\"text\"		name=\"username\">";
+		page_out += "</div>";
+
+		page_out += "<div class=\"form-field\">";
+		page_out += "<label for=\"email\">email<label><br/>";
+		page_out += "<input id=\"email\"			autocomplete=\"email\"			type=\"email\"		name=\"email\">";
+		page_out += "</div>";
+
+		page_out += "<div class=\"form-field\">";
+		page_out += "<label for=\"new-password\">password</label><br/>";
+		page_out += "<input id=\"new-password\"	autocomplete=\"new-password\"	type=\"password\"	name=\"password\">"; // See https://web.dev/sign-in-form-best-practices/#new-password
+		page_out += "</div>";
+
 		page_out += "<input type=\"submit\" value=\"Sign Up\">";
 		page_out += "</form>";
 	}
@@ -394,7 +416,12 @@ void renderResetPasswordPage(const web::RequestInfo& request_info, web::ReplyInf
 		page_out += "<div class=\"msg\" style=\"background-color: yellow\">" + msg.HTMLEscaped() + "</div>  \n";
 
 	page_out += "<form action=\"reset_password_post\" method=\"post\">";
-	page_out += "Enter your email or username: <input type=\"text\" name=\"username\"><br>";
+
+	page_out += "<div class=\"form-field\">";
+	page_out += "<label for=\"username\">Enter your email or username</label><br/>";
+	page_out += "<input id=\"username\" autocomplete=\"username\" type=\"text\" name=\"username\">";
+	page_out += "</div>";
+
 	page_out += "<input type=\"submit\" value=\"Reset password\">";
 	page_out += "</form>";
 
@@ -651,8 +678,17 @@ void renderChangePasswordPage(ServerAllWorldsState& world_state, const web::Requ
 		}
 
 		page_out += "<form action=\"change_password_post\" method=\"post\">";
-		page_out += "Enter current password: <input type=\"password\" name=\"current_password\"><br>";
-		page_out += "Enter a new password: <input type=\"password\" name=\"new_password\"><br>";
+
+		page_out += "<div class=\"form-field\">";
+		page_out += "<label for=\"current-password\">Enter current password:</label><br/>";
+		page_out += "<input id=\"current-password\" type=\"password\" autocomplete=\"current-password\" name=\"current_password\">";
+		page_out += "</div>";
+
+		page_out += "<div class=\"form-field\">";
+		page_out += "<label for=\"new-password\">Enter a new password:</label><br/>";
+		page_out += "<input id=\"new-password\" type=\"password\" autocomplete=\"new-password\" name=\"new_password\">";
+		page_out += "</div>";
+
 		page_out += "<input type=\"submit\" value=\"Set new password\">";
 		page_out += "</form>";
 
