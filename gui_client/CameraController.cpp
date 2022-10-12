4beb49c5acb4c3ac017d12d86de189cbe2874b4a
Nicholas Chapman <nick@indigorenderer.com>
2022-10-12 18:49:44 +1300

    Removed some old code from CameraController, cleaned it up a bit.

--- gui_client/CameraController.cpp
+++ gui_client/CameraController.cpp
@@ -1,18 +1,18 @@
 /*=====================================================================
 CameraController.cpp
 --------------------
-Copyright Glare Technologies Limited 2010 -
-Generated at Thu Dec 09 17:24:15 +1300 2010
+Copyright Glare Technologies Limited 2022 -
 =====================================================================*/
 #include "CameraController.h"
 
-#include <math.h>
 
-#include "../maths/mathstypes.h"
-#include "../maths/GeometrySampling.h"
+#include <maths/mathstypes.h>
+#include <maths/GeometrySampling.h>
+#include <maths/matrix3.h>
+#include <utils/ConPrint.h>
+#include <utils/StringUtils.h>
 #include <algorithm>
-#include "../utils/ConPrint.h"
-#include "../utils/StringUtils.h"
+#include <math.h>
 
 
 static const double cap = 1.0e-4;
@@ -20,8 +20,6 @@ static const double cap = 1.0e-4;
 
 CameraController::CameraController()
 {
-	target_pos = Vec3d(0.0);
-
 	base_move_speed   = 0.035;
 	base_rotate_speed = 0.005;
 
@@ -30,7 +28,6 @@ CameraController::CameraController()
 
 	invert_mouse = false;
 	invert_sideways_movement = false;
-	allow_pitching = true;
 
 	third_person = true;
 	third_person_cam_position = Vec3d(0,0,2);
@@ -51,9 +48,7 @@ CameraController::CameraController()
 
 
 CameraController::~CameraController()
-{
-
-}
+{}
 
 
 void CameraController::initialise(const Vec3d& cam_pos, const Vec3d& cam_forward, const Vec3d& cam_up, double lens_sensor_dist_, double lens_shift_up_, double lens_shift_right_)
@@ -83,9 +78,6 @@ void CameraController::initialise(const Vec3d& cam_pos, const Vec3d& cam_forward
 
 	rotation.z = atan2(rollplane_y, rollplane_x);
 
-	if(!allow_pitching)
-		rotation.y = Maths::pi_2<double>();
-
 	initial_rotation = rotation;
 }
 
@@ -103,9 +95,6 @@ void CameraController::update(const Vec3d& pos_delta, const Vec2d& rot_delta)
 		rotation.y += rot_delta.x * -rotate_speed * (invert_mouse ? -1 : 1) * (selfie_mode ? -1 : 1);
 
 		rotation.y = std::max(cap, std::min(Maths::pi<double>() - cap, rotation.y));
-
-		if(!allow_pitching)
-			rotation.y = Maths::pi_2<double>();
 	}
 
 	// Construct camera basis.
@@ -116,88 +105,8 @@ void CameraController::update(const Vec3d& pos_delta, const Vec2d& rot_delta)
 	const Vec3d right = ::crossProduct(forwards, up);
 
 	position += right		* pos_delta.x * move_speed * sideways_dir_factor +
-		forwards	* pos_delta.y * move_speed +
-		up			* pos_delta.z * move_speed * sideways_dir_factor;
-}
-
-
-static Vec3d rotatePointAroundLine(const Vec3d& p, const Vec3d& axis_point, const Vec3d& axis_dir, double angle)
-{
-	const Vec3d p_line_space = p - axis_point;
-	double u = axis_dir.x, v = axis_dir.y, w = axis_dir.z;
-	double x = p_line_space.x, y = p_line_space.y, z = p_line_space.z;
-	const double d = dot(p_line_space, axis_dir);
-	const double cos_theta = cos(angle);
-	const double sin_theta = sin(angle);
-	Vec3d rotated_p_line_space(
-		u*d*(1 - cos_theta) + x*cos_theta + (-w*y + v*z)*sin_theta,
-		v*d*(1 - cos_theta) + y*cos_theta + ( w*x - u*z)*sin_theta,
-		w*d*(1 - cos_theta) + z*cos_theta + (-v*x + u*y)*sin_theta
-	);
-
-	return rotated_p_line_space + axis_point;
-}
-
-
-void CameraController::updateTrackball(const Vec3d& pos_delta, const Vec2d& rot_delta)
-{
-	const double rotate_speed        = base_rotate_speed * mouse_sensitivity_scale;
-	const double move_speed          = base_move_speed * mouse_sensitivity_scale * move_speed_scale;
-	const double zoom_speed          = 0.15 * base_move_speed * mouse_sensitivity_scale; // Doesn't need move_speed_scale since this is a change relative to the current zoom distance.
-	const double sideways_dir_factor = invert_sideways_movement ? -1.0 : 1.0;
-
-	// conPrint("target_pos: " + target_pos.toString());
-
-	// Construct camera basis.
-	const Vec3d forwards(sin(rotation.y) * cos(rotation.x),
-		sin(rotation.y) * sin(rotation.x),
-		cos(rotation.y));
-	const Vec3d up = getUpForForwards(forwards, initialised_up);
-	const Vec3d right = normalise(::crossProduct(forwards, up));
-
-	if(rot_delta.x != 0 || rot_delta.y != 0)
-	{
-		// Rotate camera around target point
-		const double dphi = rot_delta.y * -rotate_speed;
-		const double unclamped_dtheta = rot_delta.x * rotate_speed * (invert_mouse ? -1 : 1);
-
-		const double previous_rotation_y = rotation.y;
-		const double new_rotation_y = myClamp(rotation.y - unclamped_dtheta, cap, Maths::pi<double>() - cap);
-		const double dtheta = -(new_rotation_y - previous_rotation_y);
-
-		const Vec3d newpos = rotatePointAroundLine(position, target_pos, Vec3d(0,0,1), dphi);
-		const Vec3d newpos2 = rotatePointAroundLine(newpos, target_pos, right, dtheta);
-		this->position = newpos2;
-	}
-
-	// Zoom in/out towards target point
-	const Vec3d target_to_pos = position - target_pos;
-	Vec3d scaled_target_to_pos = target_to_pos * (1.0 - pos_delta.y * zoom_speed);
-
-	// Prevent zooming in too far to an object.  This helps prevent the camera passing through objects.
-	const float MIN_DIST = 5.0e-3f;
-	if(scaled_target_to_pos.length() < MIN_DIST)
-		scaled_target_to_pos = normalise(target_to_pos) * MIN_DIST;
-
-	position = target_pos + scaled_target_to_pos;
-
-	// Allow panning of camera position and target point with CTRL+middle mouse button + mouse move
-	const Vec3d dpos = right * pos_delta.x * move_speed * sideways_dir_factor + up * pos_delta.z * move_speed * sideways_dir_factor;
-	target_pos += dpos;
-	position += dpos;
-
-
-	// We want to set the camera forwards vector, such that the resulting view vector, as influenced by the lens shift amounts, points at the target.
-	// This is not an exact solution that we're computing below, but it will do.  TODO: solve exactly.
-	const double shift_up_ratio    = lens_shift_up    / lens_sensor_dist;
-	const double shift_right_ratio = lens_shift_right / lens_sensor_dist;
-	const Vec3d shifted_target = target_pos - up * shift_up_ratio * scaled_target_to_pos.length() - right * shift_right_ratio * scaled_target_to_pos.length();
-
-	// Make camera look at target point:
-	double theta, phi;
-	GeometrySampling::sphericalCoordsForDir(normalise(shifted_target - position), 1.0, theta, phi);
-	rotation.x = phi;
-	rotation.y = theta;
+				forwards	* pos_delta.y * move_speed +
+				up			* pos_delta.z * move_speed * sideways_dir_factor;
 }
 
 
@@ -209,18 +118,8 @@ Vec3d CameraController::getFirstPersonPosition() const
 
 Vec3d CameraController::getPosition() const
 {
-	return third_person ? 
-		third_person_cam_position : // (position + third_person_cam_translation) :
-		position;
+	return third_person ? third_person_cam_position : position;
 }
-
-
-//Vec3d CameraController::getPositionWithThirdPersonOffset() const
-//{
-//	return third_person ? 
-//		(position + third_person_cam_translation) :
-//		position;
-//}
 
 
 void CameraController::setPosition(const Vec3d& pos)
@@ -305,12 +204,6 @@ Vec3d CameraController::getUpVec() const
 }
 
 
-void CameraController::setAllowPitching(bool allow_pitching_)
-{
-	allow_pitching = allow_pitching_;
-}
-
-
 Vec3d CameraController::getUpForForwards(const Vec3d& forwards, const Vec3d& singular_up)
 {
 	Vec3d up_out;
@@ -348,43 +241,6 @@ void CameraController::getBasisForAngles(const Vec3d& angles_in, const Vec3d& si
 }
 
 
-void CameraController::getAxisAngleForAngles(const Vec3d& euler_angles_in, Vec3d& axis_out, double& angle_out)
-{
-	// http://www.euclideanspace.com/maths/geometry/rotations/conversions/eulerToAngle/
-
-	const double heading = euler_angles_in.x;
-	const double attitude = 0; // TEMP HACK Maths::pi_2<float>() - euler_angles_in.y;
-	const double bank = euler_angles_in.z;
-
-	const double c1 = cos(heading * 0.5);
-	const double c2 = cos(attitude * 0.5);
-	const double c3 = cos(bank * 0.5);
-	const double s1 = sin(heading * 0.5);
-	const double s2 = sin(attitude * 0.5);
-	const double s3 = sin(bank * 0.5);
-
-	angle_out = 2 * std::acos(c1*c2*c3 - s1*s2*s3);
-
-	const Vec3d axis(
-		s1 * s2 * c3 + c1 * c2 * s3,
-		s1 * c2 * c3 + c1 * s2 * s3,
-		c1 * s2 * c3 - s1 * c2 * s3
-	);
-
-	const double len = axis.length();
-	if(len == 0)
-		axis_out = Vec3d(1,0,0);
-	else
-		axis_out = axis / len;
-}
-
-
-void CameraController::setTargetPos(const Vec3d& p)
-{ 
-	target_pos = p;
-}
-
-
 void CameraController::handleScrollWheelEvent(float delta_y)
 {
 	if(thirdPersonEnabled())
@@ -402,12 +258,6 @@ void CameraController::setSelfieModeEnabled(double cur_time, bool enabled)
 
 	start_transition_time = cur_time;
 	end_transition_time = cur_time + 1;
-}
-
-
-float CameraController::getCurrentCamRotZ(double cur_time) const
-{
-	return 0;
 }
 
 
@@ -447,14 +297,6 @@ void CameraController::test()
 	// Apply a rotation along z (roll) of 90 degrees, or pi/4 radians
 	angles.z = -NICKMATHS_PI_2;
 	CameraController::getBasisForAngles(angles, Vec3d(0, 1, 0), r, u, f);
-
-	{
-		Vec3d axis;
-		double angle;
-		CameraController::getAxisAngleForAngles(Vec3d(0.0), axis, angle);
-		conPrint(axis.toString());
-		conPrint(toString(angle));
-	}
 }
 
 #endif // BUILD_TESTS
--- gui_client/CameraController.h
+++ gui_client/CameraController.h
@@ -1,16 +1,13 @@
 /*=====================================================================
 CameraController.h
 ------------------
-Copyright Glare Technologies Limited 2010 -
-Generated at Thu Dec 09 17:24:15 +1300 2010
+Copyright Glare Technologies Limited 2022 -
 =====================================================================*/
 #pragma once
 
 
-#include "../maths/vec3.h"
-#include "../maths/vec2.h"
-#include "../maths/matrix3.h"
-#include "../maths/Quat.h"
+#include <maths/vec3.h>
+#include <maths/vec2.h>
 
 
 /*=====================================================================
@@ -28,10 +25,7 @@ public:
 
 	void update(const Vec3d& pos_delta, const Vec2d& rot_delta);
 
-	void updateTrackball(const Vec3d& pos_delta, const Vec2d& rot_delta);
-
 	Vec3d getFirstPersonPosition() const;
-	//Vec3d getPositionWithThirdPersonOffset() const;
 	Vec3d getPosition() const; // Has third person offset if third person camera is enabled.
 	void setPosition(const Vec3d& pos);
 
@@ -50,22 +44,11 @@ public:
 	Vec3d getRightVec() const;
 	Vec3d getUpVec() const;
 
-	void setAllowPitching(bool allow_pitching);
-
 	static Vec3d getUpForForwards(const Vec3d& forwards, const Vec3d& singular_up);
 	static void getBasisForAngles(const Vec3d& angles_in, const Vec3d& singular_up, Vec3d& right_out, Vec3d& up_out, Vec3d& forward_out);
 
-	static void getAxisAngleForAngles(const Vec3d& euler_angles_in, Vec3d& axis_out, double& angle_out);
-
-	void setTargetPos(const Vec3d& p);
-
 	void setThirdPersonEnabled(bool enabled) { third_person = enabled; }
 	bool thirdPersonEnabled() const { return third_person; }
-
-	//void setThirdPersonCamTranslation(const Vec3d& v) { third_person_cam_translation = v; }
-	//Vec3d thirdPersonCamTranslation() const { return third_person_cam_translation; }
-	//void setThirdPersonCamTranslation(const Vec3d& v) { third_person_cam_translation = v; }
-	//Vec3d thirdPersonCamTranslation() const { return third_person_cam_translation; }
 
 	float getThirdPersonCamDist() const { return third_person_cam_dist; }
 
@@ -73,7 +56,6 @@ public:
 
 	void setSelfieModeEnabled(double cur_time, bool enabled);
 	bool selfieModeEnabled() const { return selfie_mode; }
-	float getCurrentCamRotZ(double cur_time) const;
 
 
 	bool invert_mouse;
@@ -82,7 +64,6 @@ public:
 	static void test();
 
 private:
-
 	Vec3d position;
 	Vec3d rotation; // Specified as (heading, pitch, roll).
 					// heading is phi: rotation angle in x-y plane, from x-axis towards y-axis.
@@ -90,16 +71,11 @@ private:
 
 	Vec3d initialised_up;
 
-	Vec3d target_pos; // Target point for trackball-style navigation
-
 	Vec3d initial_rotation;
 
 	double base_move_speed, base_rotate_speed;
 	double move_speed_scale, mouse_sensitivity_scale;
 	double lens_sensor_dist, lens_shift_up, lens_shift_right;
-
-	// Spherical camera doesn't allow looking up or down. So for spherical camera, allow_pitching should be set to false.
-	bool allow_pitching;
 
 	bool third_person;
 
@@ -112,7 +88,6 @@ private:
 
 	float third_person_cam_dist;
 public:
-	//Vec3d third_person_cam_translation;
 	Vec3d third_person_cam_position;
 
 	Vec3d current_third_person_target_pos; // Blended position
