4f07c642209aed15a6f4bc99404757080ab32b4c
Nicholas Chapman <nick@indigorenderer.com>
2022-11-29 14:28:49 +1300

    Add package.json and rollup.worker.config.mjs, for using rollup to build MeshLoaderWorker.js for firefox.

---
+++ webclient/rollup.worker.config.mjs
@@ -0,0 +1,10 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'loader/MeshLoaderWorker.ts',
+  output: {
+    dir: 'dist',  # Rollup complains that this must be in the typescript outDir dir, so we can't set it to dist/loader.
+    format: 'iife'
+  },
+  plugins: [typescript()]
+}
