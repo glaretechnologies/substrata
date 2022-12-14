fd74f79cc9b348495d616c356d9b210d08da1aa8
Nicholas Chapman <nick@indigorenderer.com>
2022-12-14 15:09:05 +1300

    Removed redundant creation of Jolt floor body.

--- gui_client/PhysicsWorld.cpp
+++ gui_client/PhysicsWorld.cpp
@@ -231,28 +231,6 @@ PhysicsWorld::PhysicsWorld(/*PhysicsWorldBodyActivationCallbacks* activation_cal
 	// Registering one is entirely optional.
 	//MyContactListener contact_listener;
 	//physics_system.SetContactListener(&contact_listener);
-
-	// The main way to interact with the bodies in the physics system is through the body interface. There is a locking and a non-locking
-	// variant of this. We're going to use the locking version (even though we're not planning to access bodies from multiple threads)
-	JPH::BodyInterface& body_interface = physics_system->GetBodyInterface();
-
-	// Next we can create a rigid body to serve as the floor, we make a large box
-	// Create the settings for the collision volume (the shape). 
-	// Note that for simple shapes (like boxes) you can also directly construct a BoxShape.
-	JPH::BoxShapeSettings floor_shape_settings(JPH::Vec3(100.0f, 100.0f, 1.0f));
-
-	// Create the shape
-	JPH::ShapeSettings::ShapeResult floor_shape_result = floor_shape_settings.Create();
-	JPH::ShapeRefC floor_shape = floor_shape_result.Get(); // We don't expect an error here, but you can check floor_shape_result for HasError() / GetError()
-
-	// Create the settings for the body itself. Note that here you can also set other properties like the restitution / friction.
-	JPH::BodyCreationSettings floor_settings(floor_shape, JPH::Vec3(0.0f, 0.0f, -1.0f), JPH::Quat::sIdentity(), JPH::EMotionType::Static, Layers::NON_MOVING);
-
-	// Create the actual rigid body
-	JPH::Body* floor = body_interface.CreateBody(floor_settings); // Note that if we run out of bodies this can return nullptr
-
-	// Add it to the world
-	body_interface.AddBody(floor->GetID(), JPH::EActivation::DontActivate);
 #endif
 }
 
