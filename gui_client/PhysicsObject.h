61dac61dc2e0bb2502a5ce16a5d0ade5855d9dd4
Nicholas Chapman <nick@indigorenderer.com>
2022-12-21 16:01:02 +1300

    Reverted accidental commenting out of vars.

--- gui_client/PhysicsObject.h
+++ gui_client/PhysicsObject.h
@@ -75,9 +75,9 @@ public:
 	UID ob_uid; // Just for debugging.
 
 	// TODO: This state is redundant, since it is stored in Jolt as well.  Remove?
-	//Vec4f pos;
-	//Quatf rot; // Set in PhysicsWorld::think() from Jolt data
-	//Vec3f scale;
+	Vec4f pos;
+	Quatf rot; // Set in PhysicsWorld::think() from Jolt data
+	Vec3f scale;
 
 #if USE_JOLT
 	JPH::BodyID jolt_body_id;
