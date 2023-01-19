f5b09988419b0d7ebef6b90bde696e6c5f586d3a
Nicholas Chapman <nick@indigorenderer.com>
2023-01-19 15:12:10 +1300

    Added DiagnosticsWidget class.

---
+++ gui_client/DiagnosticsWidget.cpp
@@ -0,0 +1,24 @@
+/*=====================================================================
+DiagnosticsWidget.cpp
+---------------------
+Copyright Glare Technologies Limited 2022 -
+=====================================================================*/
+#include "DiagnosticsWidget.h"
+
+
+#include <QtCore/QSettings>
+#include <qt/QtUtils.h>
+#include <StringUtils.h>
+
+
+DiagnosticsWidget::DiagnosticsWidget(
+	QWidget* parent
+)
+{
+	setupUi(this);
+}
+
+
+DiagnosticsWidget::~DiagnosticsWidget()
+{
+}
---
+++ gui_client/DiagnosticsWidget.h
@@ -0,0 +1,28 @@
+/*=====================================================================
+DiagnosticsWidget.h
+-------------------
+Copyright Glare Technologies Limited 2022 -
+=====================================================================*/
+#pragma once
+
+
+#include "ui_DiagnosticsWidget.h"
+#include <string>
+
+class QSettings;
+
+
+/*=====================================================================
+DiagnosticsWidget
+-----------------
+
+=====================================================================*/
+class DiagnosticsWidget : public QWidget, public Ui_DiagnosticsWidget
+{
+	Q_OBJECT
+public:
+	DiagnosticsWidget(QWidget* parent);
+	~DiagnosticsWidget();
+
+private:
+};
