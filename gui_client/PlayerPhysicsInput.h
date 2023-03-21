96686f1ff3ef84cf164b891422507a8fb5152d25
Nicholas Chapman <nick@indigorenderer.com>
2023-03-21 20:37:48 +1300

    Fixed PlayerPhysicsInput not being cleared in some cases.

--- gui_client/PlayerPhysicsInput.h
+++ gui_client/PlayerPhysicsInput.h
@@ -14,6 +14,11 @@ PlayerPhysicsInput
 class PlayerPhysicsInput
 {
 public:
+	PlayerPhysicsInput()
+	{
+		clear();
+	}
+
 	void clear()
 	{
 		SHIFT_down = CTRL_down = A_down = W_down = S_down = D_down = space_down = C_down = left_down = right_down = up_down = down_down = false;
