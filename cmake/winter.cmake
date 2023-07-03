f3b1e0fd11df8d0f5fdfec7cc795f645e0ba1895
Nicholas Chapman <nick@indigorenderer.com>
2023-07-03 19:20:07 +1200

    Setting WINTER_LLVM_DIR.

--- cmake/winter.cmake
+++ cmake/winter.cmake
@@ -3,6 +3,7 @@ include_directories("${winterdir}")
 # Set variables that embed_winter.cmake requires
 set(WINTER_DIR ${winterdir})
 set(WINTER_LLVM_VERSION ${SUBSTRATA_LLVM_VERSION})
+set(WINTER_LLVM_DIR ${llvmdir})
 set(WINTER_USE_OPENCL FALSE)
 set(WINTER_INCLUDE_TEST TRUE)
 
