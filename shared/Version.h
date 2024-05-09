115dc7d32c050d7f7a3ce57420f4d374e5279db7
Ono-Sendai <nick@indigorenderer.com>
2024-05-09 23:14:37 +1200

    Bumped version to 1.2.20.

--- shared/Version.h
+++ shared/Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
 //NOTE: The ruby script builddist.rb depends on the exact layout of the following line, be careful when changing.
-const std::string cyberspace_version = "1.2.19";
+const std::string cyberspace_version = "1.2.20";
 
