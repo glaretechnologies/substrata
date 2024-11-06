4942f01bcb5bdf48e9cfa0a525ab27c52badf8ea
Ono-Sendai <nick@indigorenderer.com>
2024-11-07 02:35:02 +1300

    Using 5.2.0-opt3 branch with returning of submerged_volume.

--- scripts/get_libs.rb
+++ scripts/get_libs.rb
@@ -28,13 +28,13 @@ end
 
 #------------------- Jolt ----------------------
 begin
-	tag_dir = $glare_core_libs_dir + "/jolt/5.2.0-opt2"
+	tag_dir = $glare_core_libs_dir + "/jolt/5.2.0-opt3"
 
 	puts "Checking for already cloned source at '" + tag_dir + "'..."
 
 	if File.exist?(tag_dir)
 		puts "Already cloned there, nothing to do."
 	else
-		print_and_exec_command("git clone --depth 1 --branch 5.2.0-opt2 https://github.com/Ono-Sendai/JoltPhysics \"#{tag_dir}\"")
+		print_and_exec_command("git clone --depth 1 --branch 5.2.0-opt3 https://github.com/Ono-Sendai/JoltPhysics \"#{tag_dir}\"")
 	end
 end
