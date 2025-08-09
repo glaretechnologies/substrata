39a0ea3c2246c96d327235b2e970ee49670b4346
Ono-Sendai <nick@indigorenderer.com>
2025-08-10 02:58:16 +1200

    Added Photo class.

---
+++ server/Photo.cpp
@@ -0,0 +1,98 @@
+/*=====================================================================
+Photo.cpp
+---------
+Copyright Glare Technologies Limited 2025 -
+=====================================================================*/
+#include "Photo.h"
+
+
+#include <utils/RandomAccessInStream.h>
+#include <utils/RandomAccessOutStream.h>
+#include <utils/Exception.h>
+#include <utils/StringUtils.h>
+
+
+Photo::Photo()
+{
+	flags = 0;
+}
+
+
+Photo::~Photo()
+{}
+
+
+static const uint32 PHOTO_SERIALISATION_VERSION = 1;
+
+
+void Photo::writeToStream(RandomAccessOutStream& stream) const
+{
+	// Write to stream with a length prefix.  Do this by writing to the stream, them going back and writing the length of the data we wrote.
+	// Writing a length prefix allows for adding more fields later, while retaining backwards compatibility with older code that can just skip over the new fields.
+
+	const size_t initial_write_index = stream.getWriteIndex();
+
+	stream.writeUInt32(PHOTO_SERIALISATION_VERSION);
+	stream.writeUInt32(0); // Size of buffer will be written here later
+
+	
+	stream.writeUInt64(id);
+	::writeToStream(creator_id, stream);
+	::writeToStream(parcel_id, stream);
+	created_time.writeToStream(stream);
+	stream.writeData(&cam_pos, sizeof(cam_pos));
+	stream.writeData(&cam_angles, sizeof(cam_angles));
+	runtimeCheck(caption.size() <= 10000);
+	stream.writeStringLengthFirst(caption);
+	stream.writeUInt32(flags);
+
+	runtimeCheck(world_name.size() <= 10000);
+	stream.writeStringLengthFirst(world_name);
+	
+	runtimeCheck(local_filename.size() <= 10000);
+	stream.writeStringLengthFirst(local_filename);
+	
+	runtimeCheck(local_thumbnail_filename.size() <= 10000);
+	stream.writeStringLengthFirst(local_thumbnail_filename);
+
+	runtimeCheck(local_midsize_filename.size() <= 10000);
+	stream.writeStringLengthFirst(local_midsize_filename);
+
+	
+	// Go back and write size of buffer to buffer size field
+	const uint32 buffer_size = (uint32)(stream.getWriteIndex() - initial_write_index);
+
+	std::memcpy(stream.getWritePtrAtIndex(initial_write_index + sizeof(uint32)), &buffer_size, sizeof(uint32));
+}
+
+
+void readPhotoFromStream(RandomAccessInStream& stream, Photo& photo)
+{
+	const size_t initial_read_index = stream.getReadIndex();
+
+	const uint32 version = stream.readUInt32();
+	const size_t buffer_size = stream.readUInt32();
+
+	checkProperty(buffer_size >= 8ul, "readPhotoFromStream: buffer_size was too small");
+	checkProperty(buffer_size <= 1000000ul, "readPhotoFromStream: buffer_size was too large");
+
+	photo.id = stream.readUInt64();
+	photo.creator_id = ::readUserIDFromStream(stream);
+	photo.parcel_id = ::readParcelIDFromStream(stream);
+	photo.created_time.readFromStream(stream);
+	stream.readData(&photo.cam_pos, sizeof(photo.cam_pos));
+	stream.readData(&photo.cam_angles, sizeof(photo.cam_angles));
+	photo.caption = stream.readStringLengthFirst(10000);
+	photo.flags = stream.readUInt32();
+	photo.world_name = stream.readStringLengthFirst(10000);
+
+	photo.local_filename = stream.readStringLengthFirst(10000);
+	photo.local_thumbnail_filename = stream.readStringLengthFirst(10000);
+	photo.local_midsize_filename = stream.readStringLengthFirst(10000);
+
+
+	// Discard any remaining unread data
+	const size_t read_B = stream.getReadIndex() - initial_read_index; // Number of bytes we have read so far
+	if(read_B < buffer_size)
+		stream.advanceReadIndex(buffer_size - read_B);
+}
---
+++ server/Photo.h
@@ -0,0 +1,67 @@
+/*=====================================================================
+Photo.h
+-------
+Copyright Glare Technologies Limited 2025 -
+=====================================================================*/
+#pragma once
+
+
+#include "../shared/UserID.h"
+#include "../shared/ParcelID.h"
+#include "../maths/vec3.h"
+#include <utils/TimeStamp.h>
+#include <utils/ThreadSafeRefCounted.h>
+#include <utils/Reference.h>
+#include <utils/DatabaseKey.h>
+class RandomAccessInStream;
+class RandomAccessOutStream;
+
+
+/*=====================================================================
+Photo
+-----
+A photo / screenshot that a user captured in the substrata client photo mode, 
+and then uploaded to the server.
+=====================================================================*/
+class Photo : public ThreadSafeRefCounted
+{
+public:
+	Photo();
+	~Photo();
+
+	void writeToStream(RandomAccessOutStream& stream) const;
+
+	static const size_t MAX_CAPTION_SIZE = 10000;
+	static const size_t MAX_WORLD_NAME_SIZE = 1000;
+
+	uint64 id;
+	UserID creator_id;
+	ParcelID parcel_id;
+	TimeStamp created_time;
+	Vec3d cam_pos;
+	Vec3d cam_angles;
+	std::string caption;
+	uint32 flags;
+	std::string world_name;
+
+	std::string local_filename; // file name on disk of full resolution saved photo
+	std::string local_thumbnail_filename; // file name on disk of thumbnail (200 px wide)
+	std::string local_midsize_filename; // file name on disk of mid-size image (max 1000px wide)
+
+	DatabaseKey database_key;
+};
+
+
+typedef Reference<Photo> PhotoRef;
+
+
+void readPhotoFromStream(RandomAccessInStream& stream, Photo& s);
+
+
+struct PhotoRefHash
+{
+	size_t operator() (const PhotoRef& ob) const
+	{
+		return (size_t)ob.ptr() >> 3; // Assuming 8-byte aligned, get rid of lower zero bits.
+	}
+};
