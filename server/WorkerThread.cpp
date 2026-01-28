32b2e6f58957eaac756e0670dc022b4535b3f4e7
Ono-Sendai <nick@indigorenderer.com>
2026-01-28 20:32:33 +1300

    Fixed up some whitespace etc.

--- server/WorkerThread.cpp
+++ server/WorkerThread.cpp
@@ -2909,7 +2909,7 @@ void WorkerThread::doRun()
 								{
 									msg_to_client = "Server is in read-only mode, you can't sign up right now.";
 								}
-								else if (!server->config.enable_registration)
+								else if(!server->config.enable_registration)
 								{
 									msg_to_client = "Server is not currently accepting new registrations.";
 								}
