ba333e6bc41871347869b3f815fb8dfbf42c6db2
Ono-Sendai <nick@indigorenderer.com>
2026-02-08 23:06:07 +1300

    Tweaked image transition js.

--- webserver_public_files/root-page.js
+++ webserver_public_files/root-page.js
@@ -5,20 +5,22 @@
 // Read image paths from 'top-image-paths' element in root_page.htmlfrag
 var imgArray = document.getElementById("top-image-paths").textContent.split(',').map(s => s.trim());
 
-var curIndex = 0;
-var imgDuration = 7000;
+if(imgArray.length > 1) {
+	var curIndex = 0;
+	var imgDuration = 7000;
 
-function transitionImage() {
-    curIndex++;
-    if (curIndex == imgArray.length) { curIndex = 0; }
+	function transitionImage() {
+		curIndex++;
+		if (curIndex == imgArray.length) { curIndex = 0; }
 
-    document.getElementById('top-image').className = "top-image fadeOut"; // Start fading out by adding fadeOut class 
-    setTimeout(function () { // Once we have faded out, change to next image.
-        document.getElementById('top-image').src = imgArray[curIndex]; // Change to next image
-        document.getElementById('top-image').className = "top-image"; // Remove fadeOut class
-    }, 2000); // NOTE: this timeout value should match the time of the transition setting in #top-image in main.css.
-   
-    setTimeout(transitionImage, imgDuration);
+		document.getElementById('top-image').className = "top-image fadeOut"; // Start fading out by adding fadeOut class 
+		setTimeout(function () { // Once we have faded out, change to next image.
+			document.getElementById('top-image').src = imgArray[curIndex]; // Change to next image
+			document.getElementById('top-image').className = "top-image"; // Remove fadeOut class
+		}, 2000); // NOTE: this timeout value should match the time of the transition setting in #top-image in main.css.
+	   
+		setTimeout(transitionImage, imgDuration);
+	}
+
+	setTimeout(transitionImage, imgDuration - 2000);
 }
-
-setTimeout(transitionImage, imgDuration - 2000);
