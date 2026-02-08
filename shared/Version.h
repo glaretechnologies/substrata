8517998b670f860f5fe2d228a46f638383addd95
Ono-Sendai <nick@indigorenderer.com>
2026-02-08 23:27:13 +1300

    Bumped version to 1.6.28.

--- shared/Version.h
+++ shared/Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
 //NOTE: The ruby script builddist.rb depends on the exact layout of the following line, be careful when changing.
-const std::string cyberspace_version = "1.6.27";
+const std::string cyberspace_version = "1.6.28";
 
